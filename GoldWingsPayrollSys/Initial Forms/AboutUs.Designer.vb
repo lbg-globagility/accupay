cfd6b8338d992d45fc7b9e553ec1393e89ceb9a0
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-06-22 12:38:53 +0800

    Add AboutUs page

---
+++ GoldWingsPayrollSys/Initial Forms/AboutUs.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutUs
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblVersion = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblVersion
+        '
+        Me.lblVersion.AutoSize = True
+        Me.lblVersion.Location = New System.Drawing.Point(12, 170)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(83, 13)
+        Me.lblVersion.TabIndex = 0
+        Me.lblVersion.Text = "VersionLocation"
+        '
+        'AboutUs
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 192)
+        Me.Controls.Add(Me.lblVersion)
+        Me.Name = "AboutUs"
+        Me.Text = "AboutUs"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblVersion As System.Windows.Forms.Label
+End Class
---
+++ GoldWingsPayrollSys/Initial Forms/AboutUs.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Configuration
+
+Public Class AboutUs
+
+    Private Sub AboutUs_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim appSettings = ConfigurationManager.AppSettings
+
+        Dim versionNumber = appSettings.Get("payroll.version")
+        lblVersion.Text = "Version " & versionNumber
+    End Sub
+
+End Class
