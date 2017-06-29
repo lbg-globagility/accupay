822b1886f217edb124a43ba58b958afbd9ad5e1d
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-06-29 17:49:04 +0800

    Add SqlReader extensions

---
+++ GoldWingsPayrollSys/Core Forms/user_predefines/SqlReaderExtensions.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module SqlReaderExtensions
+    Public Function ConvertToType(Of T)(value As Object) As T
+        If IsDBNull(value) Then
+            Return CType(Nothing, T)
+        End If
+
+        Dim innerType = Nullable.GetUnderlyingType(GetType(T))
+
+        If innerType Is Nothing Then
+            Return CType(value, T)
+        Else
+            Return CTypeDynamic(value, innerType)
+        End If
+    End Function
+
+    <Extension()>
+    Public Function GetValue(Of T)(reader As IDataReader, name As String) As T
+        Return ConvertToType(Of T)(reader(name))
+    End Function
+End Module
+
