97c40fcf3e28ea0576a4b51b350240d39b451d25
Aaron del Rosario <adelrosario.globagility@gmail.com>
2017-08-04 17:00:56 +0800

    init TimeUtilityTest

--- TestPayrollSystem/TimeUtilityTest.vb
+++ TestPayrollSystem/TimeUtilityTest.vb
@@ -1,11 +1,53 @@
 ﻿Imports System.Text
-Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports GoldWingsPayrollSys
 
 <TestFixture>
 Public Class TimeUtilityTest
 
     <Test>
-    Public Sub Create_Combines_Date_And_TimeSpan_Correctly()
+    Public Sub Create_Should_Combine_Date_And_TimeSpan()
+        Dim day = Date.Parse("2017-01-01")
+        Dim time = TimeSpan.Parse("10:30:12")
+
+        Dim expected = Date.Parse("2017-01-01 10:30:12")
+        Dim answer = TimeUtility.Create(day, time)
+
+        Assert.AreEqual(expected, answer)
+    End Sub
+
+    <Test>
+    Public Sub Range_Start_Works_For_OneDay()
+        Dim day = Date.Parse("2017-01-01")
+        Dim time = TimeSpan.Parse("10:30:12")
+
+        Dim expected = Date.Parse("2017-01-01 10:30:12")
+        Dim answer = TimeUtility.RangeStart(time, day)
+
+        Assert.AreEqual(expected, answer)
+    End Sub
+
+    <Test>
+    Public Sub Range_End_Works_For_OneDay()
+        Dim day = Date.Parse("2017-01-01")
+        Dim timeIn = TimeSpan.Parse("10:30:12")
+        Dim timeOut = TimeSpan.Parse("16:15:00")
+
+        Dim expected = Date.Parse("2017-01-01 16:15:00")
+        Dim answer = TimeUtility.RangeEnd(timeIn, timeOut, day)
+
+        Assert.AreEqual(expected, answer)
+    End Sub
+
+    <Test>
+    Public Sub Range_End_Works_Across_TwoDays()
+        Dim day = Date.Parse("2017-01-01")
+        Dim timeIn = TimeSpan.Parse("16:00:00")
+        Dim timeOut = TimeSpan.Parse("02:00:00")
+
+        Dim expected = Date.Parse("2017-01-02 02:00:00")
+        Dim answer = TimeUtility.RangeEnd(timeIn, timeOut, day)
+
+        Assert.AreEqual(expected, answer)
     End Sub
 
 End Class
