53a8036620b47e97ab701a1abc7a183691abea45
Aaron del Rosario <adelrosario.globagility@gmail.com>
2017-08-04 16:41:29 +0800

    Initialize test system

---
+++ TestPayrollSystem/TimeUtilityTest.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestFixture>
+Public Class TimeUtilityTest
+
+    <Test>
+    Public Sub Create_Combines_Date_And_TimeSpan_Correctly()
+    End Sub
+
+End Class
