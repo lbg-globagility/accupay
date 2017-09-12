b14267494fcc246db68f3ea56d24a975d71cbe6b
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-09-12 10:19:52 +0800

    Init LoanSchedule and LoanTransaction entities

---
+++ GoldWingsPayrollSys/Core Forms/Loans/LoanSchedule.vb
@@ -0,0 +1,46 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace Global.PayrollSys
+
+    <Table("employeeloanschedule")>
+    Public Class LoanSchedule
+
+        Public Property RowID As Integer?
+
+        Public Property OrganizationID As Integer?
+
+        Public Property EmployeeID As Integer?
+
+        Public Property LoanTypeID As Integer?
+
+        Public Property BonusID As Integer?
+
+        Public Property LoanNumber As String
+
+        Public Property DedEffectiveDateFrom As Date
+
+        Public Property DedEffectiveDateTo As Date
+
+        Public Property TotalLoanAmount As Decimal
+
+        Public Property DeductionSchedule As String
+
+        Public Property DeductionAmount As Decimal
+
+        Public Property TotalBalanceLeft As Decimal
+
+        Public Property Status As String
+
+        Public Property DeductionPercentage As Decimal
+
+        Public Property NoOfPayPeriod As Decimal
+
+        Public Property LoanPayPeriodLeft As Decimal
+
+        Public Property Comments As String
+
+        Public Property LoanName As String
+
+    End Class
+
+End Namespace
---
+++ GoldWingsPayrollSys/Core Forms/Loans/LoanTransaction.vb
@@ -0,0 +1,33 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+
+Namespace Global.PayrollSys
+
+    <Table("scheduledloansperpayperiod")>
+    Public Class LoanTransaction
+
+        Public Property RowID As Integer?
+
+        Public Property Created As Date
+
+        Public Property CreatedBy As Integer?
+
+        Public Property LastUpd As Date
+
+        Public Property LastUpdBy As Integer?
+
+        Public Property OrganizatonID As Integer?
+
+        Public Property PayPeriod As Integer?
+
+        Public Property EmployeeID As Integer?
+
+        Public Property LoanScheduleID As Integer?
+
+        Public Property LoanPayPeriodLeft As Integer
+
+        Public Property TotalBalanceLeft As Decimal
+
+    End Class
+
+End Namespace
