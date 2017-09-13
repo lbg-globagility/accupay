771522d06e835df8a09ea86c8afd6edea5a9bda2
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-09-13 17:22:58 +0800

    Change connectionString for PayrollContext

--- GoldWingsPayrollSys/Core Forms/Commons/PayrollContext.vb
+++ GoldWingsPayrollSys/Core Forms/Commons/PayrollContext.vb
@@ -1,5 +1,7 @@
+
 Imports System
 Imports System.Data.Entity
+Imports System.Data.Common
 Imports System.Linq
 
 Imports PayrollSys
@@ -7,19 +9,15 @@ Imports PayrollSys
 Public Class PayrollContext
     Inherits DbContext
 
-    ' Your context has been configured to use a 'PayrollContext' connection string from your application's 
-    ' configuration file (App.config or Web.config). By default, this connection string targets the 
-    ' 'GoldWingsPayrollSys.PayrollContext' database on your LocalDb instance. 
-    ' 
-    ' If you wish to target a different database and/or database provider, modify the 'PayrollContext' 
-    ' connection string in the application configuration file.
-    Public Sub New()
-        MyBase.New("name=PayrollContext")
-    End Sub
+    Private Shared Function GetConnection() As DbConnection
+        Dim connection = DbProviderFactories.GetFactory("MySql.Data.MySqlClient").CreateConnection()
+        connection.ConnectionString = connectionString
+        Return connection
+    End Function
 
-    ' Add a DbSet for each entity type that you want to include in your model. For more information 
-    ' on configuring and using a Code First model, see http:'go.microsoft.com/fwlink/?LinkId=390109.
-    ' Public Overridable Property MyEntities() As DbSet(Of MyEntity)
+    Public Sub New()
+        MyBase.New(GetConnection(), True)
+    End Sub
 
     Public Overridable Property LoanSchedules As DbSet(Of LoanSchedule)
 
