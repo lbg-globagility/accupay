343d8cd6dc1ed9bdea1e980c1a0a7f56e28928f3
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-09-14 12:49:26 +0800

    Init Salary class

---
+++ GoldWingsPayrollSys/Core Forms/Salary/Salary.vb
@@ -0,0 +1,50 @@
+﻿Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+
+Namespace Global.PayrollSys
+
+    <Table("employeesalary")>
+    Public Class Salary
+
+        <Key>
+        Public Property RowID As Integer?
+
+        Public Property EmployeeID As Integer?
+
+        Public Property OrganizationID As Integer?
+
+        Public Property FilingStatusID As Integer?
+
+        Public Property PaySocialSecurityID As Integer?
+
+        Public Property PayPhilHealthID As Integer?
+
+        Public Property HDMFAmount As Decimal
+
+        Public Property TrueSalary As Decimal
+
+        Public Property BasicPay As Decimal
+
+        Public Property Salary As Decimal
+
+        Public Property UndeclaredSalary As Decimal
+
+        Public Property BasicDailyPay As Decimal
+
+        Public Property BasicHourlyPay As Decimal
+
+        Public Property NoOfDependents As Integer
+
+        Public Property MaritalStatus As String
+
+        Public Property EffectiveDateFrom As Date
+
+        Public Property EffectiveDateTo As Date?
+
+        Public Property OverrideDiscardSSSContrib As Boolean
+
+        Public Property OverrideDiscaredPhilHealth As Boolean
+
+    End Class
+
+End Namespace
