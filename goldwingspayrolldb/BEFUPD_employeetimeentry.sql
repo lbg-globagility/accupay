625b569d348ebff26b88889eaec50b053abbc93f
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-09-19 17:21:01 +0800

    Backup stored procs

--- goldwingspayrolldb/BEFUPD_employeetimeentry.sql
+++ goldwingspayrolldb/BEFUPD_employeetimeentry.sql
@@ -14,25 +14,7 @@ DECLARE hasShift BOOLEAN;
 
 DECLARE dailyRate DECIMAL(11,6);
 
-DECLARE TaxableDailyAllowanceAmount DECIMAL(11,6);
-
-DECLARE rate_this_date DECIMAL(11,6);
-DECLARE isPresentInWorkingDaysPriorToThisDate CHAR(1) DEFAULT '0';
-DECLARE payrate_this_date DECIMAL(11,2);
-
-DECLARE nightDiffTimeFrom TIME DEFAULT '22:00:00';
-DECLARE nightDiffTimeTo TIME DEFAULT '06:00:00';
-
-DECLARE nightDiffRangeFrom DATETIME;
-DECLARE nightDiffRangeTo DATETIME;
-
 DECLARE leaveHours DECIMAL(15, 4);
-
-DECLARE shiftStart DATETIME;
-DECLARE shiftEnd DATETIME;
-
-DECLARE dateToday DATE;
-DECLARE dateTomorrow DATE;
 
 DECLARE isRegularHoliday BOOLEAN;
 DECLARE isSpecialNonWorkingHoliday BOOLEAN;
@@ -44,31 +26,10 @@ DECLARE isShiftRestDay BOOLEAN;
 DECLARE hasWorked BOOLEAN;
 DECLARE hasLeave BOOLEAN;
 
-SELECT
-    `PayRate`,
-    GET_employeerateperday(NEW.EmployeeID, NEW.OrganizationID, NEW.`Date`)
-FROM payrate pr
-WHERE pr.RowID=NEW.PayRateID
-INTO
-    payrate_this_date,
-    rate_this_date;
-
-SET NEW.VacationLeaveHours = IFNULL(NEW.VacationLeaveHours,0);
-SET NEW.SickLeaveHours = IFNULL(NEW.SickLeaveHours,0);
-SET NEW.MaternityLeaveHours = IFNULL(NEW.MaternityLeaveHours,0);
-SET NEW.OtherLeaveHours = IFNULL(NEW.OtherLeaveHours,0);
-
-SELECT COMPUTE_TimeDifference(sh.TimeFrom, sh.TimeTo)
-FROM employeeshift esh
-INNER JOIN shift sh
-ON sh.RowID = esh.ShiftID
-WHERE esh.EmployeeID = NEW.EmployeeID AND
-    esh.OrganizationID = NEW.OrganizationID AND
-    esh.RestDay = '0' AND
-    NEW.`Date` BETWEEN esh.EffectiveFrom AND esh.EffectiveTo
-LIMIT 1
-INTO @fullshifthrs;
-
+SET NEW.VacationLeaveHours = IFNULL(NEW.VacationLeaveHours, 0);
+SET NEW.SickLeaveHours = IFNULL(NEW.SickLeaveHours, 0);
+SET NEW.MaternityLeaveHours = IFNULL(NEW.MaternityLeaveHours, 0);
+SET NEW.OtherLeaveHours = IFNULL(NEW.OtherLeaveHours, 0);
 SET NEW.RegularHoursWorked = IFNULL(NEW.RegularHoursWorked, 0);
 SET NEW.RegularHoursAmount = IFNULL(NEW.RegularHoursAmount, 0);
 SET NEW.TotalHoursWorked = IFNULL(NEW.TotalHoursWorked, 0);
@@ -147,80 +108,6 @@ IF isDefaultRestDay = '1' && NEW.EmployeeShiftID IS NOT NULL AND COALESCE(NEW.Re
     SET NEW.EmployeeShiftID = NULL;
 END IF;
 
-SELECT GET_employeerateperday(NEW.EmployeeID, NEW.OrganizationID, NEW.`Date`)
-INTO rate_this_date;
-
-
-SELECT SUM(ea.AllowanceAmount)
-FROM employeeallowance ea
-WHERE ea.AllowanceFrequency='Daily'
-    AND ea.TaxableFlag='1'
-    AND ea.EmployeeID=NEW.EmployeeID
-    AND ea.OrganizationID=NEW.OrganizationID
-    AND NEW.`Date` BETWEEN ea.EffectiveStartDate AND ea.EffectiveEndDate
-INTO TaxableDailyAllowanceAmount;
-
-SET rate_this_date = IFNULL(
-    (
-        SELECT sh.DivisorToDailyRate - COMPUTE_TimeDifference(sh.BreakTimeFrom,sh.BreakTimeTo)
-        FROM employeeshift esh
-        INNER JOIN shift sh
-            ON sh.RowID=esh.ShiftID
-        WHERE esh.RowID=NEW.EmployeeShiftID
-    ),
-    0
-);
-
-SET @daily_salary = GET_employeerateperday(NEW.EmployeeID, NEW.OrganizationID, NEW.`Date`);
-SET @leave_hrs = (NEW.VacationLeaveHours + NEW.SickLeaveHours + NEW.MaternityLeaveHours + NEW.OtherLeaveHours);
-
-SET NEW.TaxableDailyAllowance = (
-    SELECT (
-        IF(
-            pr.PayType='Regular Day',
-            IF(
-                NEW.TotalDayPay > NEW.RegularHoursAmount AND @leave_hrs > 0,
-                IF(
-                    NEW.RegularHoursAmount=0,
-                    NEW.TotalDayPay,
-                    NEW.RegularHoursAmount
-                ),
-                IF(
-                    NEW.RegularHoursAmount > @daily_salary,
-                    @daily_salary,
-                    NEW.RegularHoursAmount
-                )
-            ),
-            IF(
-                pr.PayType='Special Non-Working Holiday' AND e.CalcSpecialHoliday = '1',
-                IF(
-                    e.EmployeeType = 'Daily',
-                    (NEW.RegularHoursAmount / pr.`PayRate`),
-                    NEW.HolidayPayAmount
-                ),
-                IF(
-                    pr.PayType='Special Non-Working Holiday' AND e.CalcSpecialHoliday = '0',
-                    IF(
-                        e.EmployeeType = 'Daily',
-                        NEW.RegularHoursAmount,
-                        NEW.HolidayPayAmount
-                    ),
-                    IF(
-                        pr.PayType='Regular Holiday' AND e.CalcHoliday = '1',
-                        NEW.HolidayPayAmount + ((NEW.VacationLeaveHours + NEW.SickLeaveHours + NEW.MaternityLeaveHours + NEW.OtherLeaveHours) * (@daily_salary / rate_this_date)),
-                        0
-                    )
-                )
-            )
-        ) / @daily_salary
-    ) * TaxableDailyAllowanceAmount
-    FROM employee e
-    INNER JOIN payrate pr
-        ON pr.RowID=NEW.PayRateID
-    WHERE e.RowID=NEW.EmployeeID
-        AND e.OrganizationID=NEW.OrganizationID
-);
-
 IF NEW.TaxableDailyAllowance IS NULL THEN
     SET NEW.TaxableDailyAllowance = 0;
 END IF;
@@ -238,9 +125,9 @@ IF NEW.LastUpd != '1900-01-01 00:00:01' THEN
     SET NEW.EmployeeSalaryID = (
         SELECT RowID
         FROM employeesalary
-        WHERE EmployeeID=NEW.EmployeeID
-            AND OrganizationID=NEW.OrganizationID
-            AND NEW.`Date` BETWEEN EffectiveDateFrom AND IFNULL(EffectiveDateTo,NEW.`Date`)
+        WHERE EmployeeID=NEW.EmployeeID AND
+            OrganizationID=NEW.OrganizationID AND
+            NEW.`Date` BETWEEN EffectiveDateFrom AND IFNULL(EffectiveDateTo,NEW.`Date`)
         LIMIT 1
     );
 
