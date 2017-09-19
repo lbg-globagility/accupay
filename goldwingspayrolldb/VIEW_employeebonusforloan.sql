eb0522b72abb99a32340f8bd1b73919422d8be52
Lambert <lbg.globagility@gmail.com>
2017-09-19 16:59:03 +0800

    updates for database objects with regards to the bonus as payment for loans

--- goldwingspayrolldb/VIEW_employeebonusforloan.sql
+++ goldwingspayrolldb/VIEW_employeebonusforloan.sql
@@ -1,16 +1,41 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.5-10.0.12-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4694
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for procedure goldwingspayrolldb_global_e_pc.VIEW_employeebonusforloan
 DROP PROCEDURE IF EXISTS `VIEW_employeebonusforloan`;
 DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `VIEW_employeebonusforloan`(IN `ebon_EmployeeID` INT, IN `ebon_OrganizationID` INT, IN `AssignBonusRowID` INT, IN `loan_startdate` DATE, IN `loan_enddate` DATE)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `VIEW_employeebonusforloan`(IN `ebon_EmployeeID` INT, IN `ebon_OrganizationID` INT, IN `AssignBonusRowID` INT, IN `loan_startdate` DATE, IN `loan_enddate` DATE, IN `els_rowid` INT)
     DETERMINISTIC
 BEGIN
 
-    SELECT FALSE
+DECLARE max_date_ofparamdate
+       ,min_date_ofparamdate DATE;
+       
+SELECT
+	MIN(pyp.PayFromDate)
+	,MAX(pyp.PayToDate)
+FROM payperiod pyp
+INNER JOIN payperiod pp
+        ON pp.OrganizationID=pyp.OrganizationID
+		     AND (pp.PayFromDate >= loan_startdate OR pp.PayToDate >= loan_startdate)
+			  AND (pp.PayFromDate <= loan_enddate OR pp.PayToDate <= loan_enddate)
+WHERE pyp.OrganizationID=ebon_OrganizationID
+AND pyp.`Year` = pp.`Year`
+INTO min_date_ofparamdate
+     ,max_date_ofparamdate;
+
+SELECT i.*
+FROM (
+    SELECT TRUE
     ,ebon.RowID
     ,IFNULL(p.PartNo,'') AS `Type`
     ,COALESCE(ebon.BonusAmount,0) AS BonusAmount
@@ -19,10 +44,17 @@ BEGIN
     ,IFNULL(DATE_FORMAT(ebon.EffectiveEndDate,'%m/%d/%Y'),'1/1/1900') AS EffectiveEndDate
     ,IFNULL(ebon.ProductID,'') AS ProductID
     ,ebon.RemainingBalance
+    ,els.Comments `Remarks` # ebon.Remarks
+    ,els.LoanPayPeriodLeftForBonus `LoanPaymentPotential`
+    ,'1st' `Grouping`
     FROM employeebonus ebon
+    INNER JOIN employeeloanschedule els
+            ON els.BonusID=ebon.RowID
+               AND els.RowID=els_rowid
     INNER JOIN product p ON ebon.ProductID=p.RowID
     WHERE ebon.EmployeeID=ebon_EmployeeID
     AND ebon.OrganizationID=ebon_OrganizationID AND ebon.RowID=AssignBonusRowID
+    
 UNION
     SELECT FALSE
     ,ebon.RowID
@@ -33,17 +65,43 @@ UNION
     ,IFNULL(DATE_FORMAT(ebon.EffectiveEndDate,'%m/%d/%Y'),'1/1/1900') AS EffectiveEndDate
     ,IFNULL(ebon.ProductID,'') AS ProductID
     ,ebon.RemainingBalance
+    ,ebon.Remarks
+    ,0 `LoanPaymentPotential`
+    ,'2nd' `Grouping`
     FROM employeebonus ebon
     INNER JOIN product p ON ebon.ProductID=p.RowID
     WHERE ebon.EmployeeID=ebon_EmployeeID
-    AND ebon.OrganizationID=ebon_OrganizationID AND ebon.RemainingBalance > 0
-    AND (ebon.EffectiveStartDate >= loan_startdate OR ebon.EffectiveEndDate >= loan_startdate)
+    AND ebon.OrganizationID=ebon_OrganizationID
+	 AND (ebon.EffectiveStartDate >= loan_startdate OR ebon.EffectiveEndDate >= loan_startdate)
     AND (ebon.EffectiveStartDate <= loan_enddate OR ebon.EffectiveEndDate <= loan_enddate)
-    AND ebon.RemainingBalance > 0;
+    AND ebon.RemainingBalance > 0
+    
+UNION
+    SELECT FALSE
+    ,ebon.RowID
+    ,IFNULL(p.PartNo,'') AS `Type`
+    ,COALESCE(ebon.BonusAmount,0) AS BonusAmount
+    ,IFNULL(ebon.AllowanceFrequency,'') AS AllowanceFrequency
+    ,IFNULL(DATE_FORMAT(ebon.EffectiveStartDate,'%m/%d/%Y'),'1/1/1900') AS EffectiveStartDate
+    ,IFNULL(DATE_FORMAT(ebon.EffectiveEndDate,'%m/%d/%Y'),'1/1/1900') AS EffectiveEndDate
+    ,IFNULL(ebon.ProductID,'') AS ProductID
+    ,ebon.RemainingBalance
+    ,ebon.Remarks
+    ,0 `LoanPaymentPotential`
+    ,'3rd' `Grouping`
+    FROM employeebonus ebon
+    INNER JOIN product p ON ebon.ProductID=p.RowID
+    WHERE ebon.EmployeeID=ebon_EmployeeID
+    AND ebon.OrganizationID=ebon_OrganizationID
+	 # AND (ebon.EffectiveStartDate >= min_date_ofparamdate OR ebon.EffectiveEndDate >= min_date_ofparamdate)
+    # AND (ebon.EffectiveStartDate <= max_date_ofparamdate OR ebon.EffectiveEndDate <= max_date_ofparamdate)
+    AND ebon.RemainingBalance > 0
+     ) i
+GROUP BY i.RowID
+;
 
 END//
 DELIMITER ;
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ goldwingspayrolldb/aftins_paystubbonus_then_aftins_bonusloandeduction.sql
@@ -0,0 +1,183 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.5-10.0.12-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4694
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for procedure goldwingspayrolldb_global_e_pc.aftins_paystubbonus_then_aftins_bonusloandeduction
+DROP PROCEDURE IF EXISTS `aftins_paystubbonus_then_aftins_bonusloandeduction`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `aftins_paystubbonus_then_aftins_bonusloandeduction`(IN `OrganizID` INT, IN `UserRowID` INT, IN `EmpRowID` INT, IN `psb_PayPeriodID` INT, IN `min_date_ofparamdate` DATE, IN `max_date_ofparamdate` DATE)
+    DETERMINISTIC
+BEGIN
+
+/* Disable first the functionalities in AFTINS_paystubbonus trigger */
+
+DECLARE IsFirstHalfOfMonth
+        ,is_bonus_loan_deduct_exists BOOL;
+
+DECLARE pay_datefrom
+        ,pay_dateto DATE;
+
+SELECT EXISTS(SELECT bld.RowID
+              FROM bonusloandeduction bld
+              INNER JOIN employeeloanschedule els
+                      ON els.RowID=bld.LoanSchedID
+                         AND els.OrganizationID=bld.OrganizationID
+                         AND els.EmployeeID=EmpRowID
+              WHERE bld.PayPeriodID=psb_PayPeriodID
+              AND bld.OrganizationID=OrganizID
+              ) `Result`
+INTO is_bonus_loan_deduct_exists;
+
+SELECT (pp.`Half` = 1) `Result`
+,pp.PayFromDate
+,pp.PayToDate
+FROM payperiod pp
+WHERE pp.RowID=psb_PayPeriodID
+INTO IsFirstHalfOfMonth
+     ,pay_datefrom
+	  ,pay_dateto;
+
+INSERT INTO bonusloandeduction
+(
+  OrganizationID
+  ,CreatedBy
+  ,LoanSchedID
+  ,PayPeriodID
+  ,DeductionLoanAmount
+) SELECT
+  OrganizID
+  ,UserRowID
+  ,i.RowID
+  ,psb_PayPeriodID
+  ,(@els_deduct_amt := i.`DeductionAmount`)
+  
+  FROM (SELECT
+        els.RowID
+        ,IF(els.LoanPayPeriodLeft = 0
+		      , ( els.DeductionAmount + (els.TotalLoanAmount - (els.DeductionAmount * els.NoOfPayPeriod)) )
+	         , els.DeductionAmount) `DeductionAmount`
+        FROM employeeloanschedule els
+		  INNER JOIN employeebonus eb
+		          ON eb.EmployeeID = EmpRowID
+					    AND eb.OrganizationID = OrganizID
+						 AND (eb.EffectiveStartDate >= pay_datefrom OR eb.EffectiveEndDate >= pay_datefrom)
+						 AND (eb.EffectiveStartDate <= pay_dateto OR eb.EffectiveEndDate <= pay_dateto)
+						 AND els.BonusPotentialPaymentForLoan = 0
+						 AND eb.RowID = els.BonusID
+        WHERE els.EmployeeID=EmpRowID
+		  AND els.OrganizationID=OrganizID
+		  AND els.`Status` IN ('In progress', 'Complete')
+		  AND els.BonusID IS NOT NULL
+		  AND (els.DedEffectiveDateFrom >= min_date_ofparamdate OR els.DedEffectiveDateTo >= min_date_ofparamdate)
+		  AND (els.DedEffectiveDateFrom <= max_date_ofparamdate OR els.DedEffectiveDateTo <= max_date_ofparamdate)
+        
+      UNION
+        SELECT
+        els.RowID
+        ,(els.TotalLoanAmount - ((els.NoOfPayPeriod - els.LoanPayPeriodLeftForBonus) * els.DeductionAmount)) `DeductionAmount`
+        FROM employeeloanschedule els
+		  INNER JOIN employeebonus eb
+		          ON eb.EmployeeID = EmpRowID
+					    AND eb.OrganizationID = OrganizID
+						 AND (eb.EffectiveStartDate >= pay_datefrom OR eb.EffectiveEndDate >= pay_datefrom)
+						 AND (eb.EffectiveStartDate <= pay_dateto OR eb.EffectiveEndDate <= pay_dateto)
+						 AND els.BonusPotentialPaymentForLoan = 1
+						 AND eb.RowID = els.BonusID
+        WHERE els.EmployeeID=EmpRowID
+		  AND els.OrganizationID=OrganizID
+		  AND els.`Status` IN ('In progress', 'Complete')
+		  AND els.BonusID IS NOT NULL
+		  AND (els.DedEffectiveDateFrom >= min_date_ofparamdate OR els.DedEffectiveDateTo >= min_date_ofparamdate)
+		  AND (els.DedEffectiveDateFrom <= max_date_ofparamdate OR els.DedEffectiveDateTo <= max_date_ofparamdate)
+        
+        ) i
+ON
+DUPLICATE
+KEY
+UPDATE
+  LastUpd=CURRENT_TIMESTAMP()
+  ,LastUpdBy=UserRowID
+  ,DeductionLoanAmount=@els_deduct_amt;
+
+
+# Bonus pays loan deduction amount
+UPDATE employeeloanschedule els
+INNER JOIN employeebonus eb
+        ON eb.EmployeeID = els.EmployeeID
+	        AND eb.OrganizationID = els.OrganizationID
+			  AND eb.RowID = els.BonusID
+
+LEFT JOIN (SELECT bld.RowID
+           ,bld.LoanSchedID
+           FROM bonusloandeduction bld
+           INNER JOIN employeeloanschedule els
+                   ON els.RowID=bld.LoanSchedID
+                      AND els.OrganizationID=bld.OrganizationID
+                      AND els.EmployeeID=EmpRowID
+							 # AND els.BonusPotentialPaymentForLoan = 0
+           WHERE bld.PayPeriodID=psb_PayPeriodID
+           AND bld.OrganizationID=OrganizID
+           ) i
+       ON i.LoanSchedID != els.RowID
+
+SET els.LoanPayPeriodLeft = IF((els.LoanPayPeriodLeft - 1) < 0, 0, (els.LoanPayPeriodLeft - 1))
+,els.TotalBalanceLeft = (els.TotalBalanceLeft - els.DeductionAmount)
+,els.LastUpdBy=UserRowID
+WHERE els.EmployeeID=EmpRowID
+AND els.OrganizationID=OrganizID
+AND els.`Status`='In progress'
+AND els.BonusID IS NOT NULL
+AND (els.DedEffectiveDateFrom >= min_date_ofparamdate OR els.DedEffectiveDateTo >= min_date_ofparamdate)
+AND (els.DedEffectiveDateFrom <= max_date_ofparamdate OR els.DedEffectiveDateTo <= max_date_ofparamdate)
+AND els.BonusPotentialPaymentForLoan = 0
+# AND is_bonus_loan_deduct_exists = FALSE
+;
+
+
+# Bonus pays full loan balance
+UPDATE employeeloanschedule els
+INNER JOIN employeebonus eb
+        ON eb.EmployeeID = els.EmployeeID
+	        AND eb.OrganizationID = els.OrganizationID
+			  AND eb.RowID = els.BonusID
+
+LEFT JOIN (SELECT bld.RowID
+           ,bld.LoanSchedID
+           FROM bonusloandeduction bld
+           INNER JOIN employeeloanschedule els
+                   ON els.RowID=bld.LoanSchedID
+                      AND els.OrganizationID=bld.OrganizationID
+                      AND els.EmployeeID=EmpRowID
+							 # AND els.BonusPotentialPaymentForLoan = 1
+           WHERE bld.PayPeriodID=psb_PayPeriodID
+           AND bld.OrganizationID=OrganizID
+           ) i
+       ON i.LoanSchedID != els.RowID
+
+SET els.LoanPayPeriodLeft = (els.LoanPayPeriodLeft - els.LoanPayPeriodLeftForBonus)
+,els.TotalBalanceLeft = (els.TotalBalanceLeft - (els.TotalLoanAmount - ((els.NoOfPayPeriod - els.LoanPayPeriodLeftForBonus) * els.DeductionAmount)))
+,els.LastUpdBy=UserRowID
+WHERE els.EmployeeID=EmpRowID
+AND els.OrganizationID=OrganizID
+AND els.`Status`='In progress'
+AND els.BonusID IS NOT NULL
+AND (els.DedEffectiveDateFrom >= min_date_ofparamdate OR els.DedEffectiveDateTo >= min_date_ofparamdate)
+AND (els.DedEffectiveDateFrom <= max_date_ofparamdate OR els.DedEffectiveDateTo <= max_date_ofparamdate)
+AND els.BonusPotentialPaymentForLoan = 1
+# AND is_bonus_loan_deduct_exists = FALSE
+;
+
+
+END//
+DELIMITER ;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
