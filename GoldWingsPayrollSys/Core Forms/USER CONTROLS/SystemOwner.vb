b55dd00cc5de0d28685358992b96b613458c0830
Lambert <lbg.globagility@gmail.com>
2017-10-05 10:18:47 +0800

    commit all necessary changes for Hyundai pay slip

---
+++ GoldWingsPayrollSys/Core Forms/USER CONTROLS/SystemOwner.vb
@@ -0,0 +1,20 @@
+﻿
+Public Class SystemOwner
+
+    Const text_goldwings As String = "Goldwings"
+
+    Const text_hyundai As String = "Hyundai"
+
+    Public Shared ReadOnly Property Goldwings As String
+        Get
+            Return text_goldwings
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property Hyundai As String
+        Get
+            Return text_hyundai
+        End Get
+    End Property
+
+End Class
