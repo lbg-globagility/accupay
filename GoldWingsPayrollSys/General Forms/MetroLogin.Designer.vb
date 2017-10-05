bc4f22a415ee378ac99cb338cfbe9b7b6bb703b0
Lambert <lbg.globagility@gmail.com>
2017-10-05 15:11:48 +0800

    add function that will set the proper interface according to current system owner of application

--- GoldWingsPayrollSys/General Forms/MetroLogin.Designer.vb
+++ GoldWingsPayrollSys/General Forms/MetroLogin.Designer.vb
@@ -37,8 +37,10 @@ Partial Class MetroLogin
         Me.lnklblobf = New System.Windows.Forms.LinkLabel()
         Me.lnklblovertime = New System.Windows.Forms.LinkLabel()
         Me.lnklblleave = New System.Windows.Forms.LinkLabel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
         CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PhotoImages, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MetroStyleManager1
@@ -148,7 +150,7 @@ Partial Class MetroLogin
         Me.lnklblobf.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblobf.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblobf.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblobf.Location = New System.Drawing.Point(20, 338)
+        Me.lnklblobf.Location = New System.Drawing.Point(0, 42)
         Me.lnklblobf.Name = "lnklblobf"
         Me.lnklblobf.Size = New System.Drawing.Size(126, 15)
         Me.lnklblobf.TabIndex = 22
@@ -161,7 +163,7 @@ Partial Class MetroLogin
         Me.lnklblovertime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblovertime.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblovertime.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblovertime.Location = New System.Drawing.Point(20, 320)
+        Me.lnklblovertime.Location = New System.Drawing.Point(0, 24)
         Me.lnklblovertime.Name = "lnklblovertime"
         Me.lnklblovertime.Size = New System.Drawing.Size(85, 15)
         Me.lnklblovertime.TabIndex = 21
@@ -174,21 +176,29 @@ Partial Class MetroLogin
         Me.lnklblleave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblleave.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblleave.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblleave.Location = New System.Drawing.Point(20, 302)
+        Me.lnklblleave.Location = New System.Drawing.Point(0, 9)
         Me.lnklblleave.Name = "lnklblleave"
         Me.lnklblleave.Size = New System.Drawing.Size(69, 15)
         Me.lnklblleave.TabIndex = 20
         Me.lnklblleave.TabStop = True
         Me.lnklblleave.Text = "Lea&ve filing"
         '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.lnklblobf)
+        Me.Panel1.Controls.Add(Me.lnklblleave)
+        Me.Panel1.Controls.Add(Me.lnklblovertime)
+        Me.Panel1.Location = New System.Drawing.Point(23, 302)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(138, 65)
+        Me.Panel1.TabIndex = 23
+        '
         'MetroLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(544, 371)
-        Me.Controls.Add(Me.lnklblobf)
-        Me.Controls.Add(Me.lnklblovertime)
-        Me.Controls.Add(Me.lnklblleave)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.MetroLink1)
         Me.Controls.Add(Me.PhotoImages)
         Me.Controls.Add(Me.MetroLabel3)
@@ -208,6 +218,8 @@ Partial Class MetroLogin
         Me.Text = "Welcome"
         CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PhotoImages, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -225,4 +237,5 @@ Partial Class MetroLogin
     Friend WithEvents lnklblobf As LinkLabel
     Friend WithEvents lnklblovertime As LinkLabel
     Friend WithEvents lnklblleave As LinkLabel
+    Friend WithEvents Panel1 As Panel
 End Class
