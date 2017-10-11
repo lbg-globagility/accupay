d3fec717ecf20187cf33dc6909ec348edbeaca83
Lambert <lbg.globagility@gmail.com>
2017-10-11 15:25:53 +0800

    add some fix to the half day leave and other half is time logs attendance

--- goldwingspayrolldb/AFTUPD_employeeleave_then_employeetimeentry.sql
+++ goldwingspayrolldb/AFTUPD_employeeleave_then_employeetimeentry.sql
@@ -1,9 +1,16 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.5-10.0.12-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             8.3.0.4694
+-- --------------------------------------------------------
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- Dumping structure for trigger hyundaipayrolldb_dev.AFTUPD_employeeleave_then_employeetimeentry
 DROP TRIGGER IF EXISTS `AFTUPD_employeeleave_then_employeetimeentry`;
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
@@ -37,12 +44,14 @@ SET reghrsworkd = IF('09:00:00' > reghrsworkd, reghrsworkd, ADDDATE(reghrsworkd,
 
 SELECT ((TIME_TO_SEC(reghrsworkd) / 60) / 60) INTO numhrsworkd;
 
-IF numhrsworkd >= 9 THEN
+/*IF numhrsworkd >= 9 THEN
     SET numhrsworkd = 8;
 
-END IF;
+END IF;*/
 
-IF OLD.LeaveStartDate = NEW.LeaveStartDate AND OLD.LeaveEndDate = NEW.LeaveEndDate THEN
+SET numhrsworkd = NEW.OfficialValidHours;
+
+/*IF OLD.LeaveStartDate = NEW.LeaveStartDate AND OLD.LeaveEndDate = NEW.LeaveEndDate THEN
 
     IF OLD.LeaveStartTime != NEW.LeaveStartTime OR OLD.LeaveEndTime != NEW.LeaveEndTime THEN
 
@@ -53,7 +62,7 @@ IF OLD.LeaveStartDate = NEW.LeaveStartDate AND OLD.LeaveEndDate = NEW.LeaveEndDa
         ,SickLeaveHours=IF(leavetype LIKE '%Sick%',numhrsworkd,0.0)
         ,MaternityLeaveHours=IF(leavetype LIKE '%aternity%',numhrsworkd,0.0)
         ,OtherLeaveHours=IF(leavetype LIKE '%Others%',numhrsworkd,0.0)
-        WHERE Date BETWEEN OLD.LeaveStartDate AND OLD.LeaveEndDate
+        WHERE `Date` BETWEEN OLD.LeaveStartDate AND OLD.LeaveEndDate
         AND OrganizationID=OLD.OrganizationID
         AND EmployeeID=OLD.EmployeeID;
 
@@ -234,9 +243,7 @@ ELSE
 
         END IF;
 
-END IF;
-
-
+END IF;*/
 
 SELECT RowID FROM `view` WHERE ViewName='Employee Leave' AND OrganizationID=NEW.OrganizationID LIMIT 1 INTO viewID;
 
@@ -299,7 +306,6 @@ END IF;
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
-
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
