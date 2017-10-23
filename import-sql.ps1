3cf7bc165058c5877c3cefff3ec3fbf60c74d62f
aaron_delrosario <adelrosario.globagility@gmail.com>
2017-10-23 10:57:27 +0800

    Fix server and password in import-sql

--- import-sql.ps1
+++ import-sql.ps1
@@ -1,13 +1,21 @@
-$pwd = Read-Host 'password';
-$database = Read-Host 'database';
 $server = Read-Host 'server';
+$database = Read-Host 'database';
+$pwd = Read-Host 'password' -AsSecureString;
+
+$pwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd));
 
 Get-ChildItem ./goldwingspayrolldb |
 Foreach-Object {
     $filename = $_.FullName;
 
-    Get-Content $filename |
-        mysql --user=root --password=$pwd --database=$database --comments;
+    If (!$server) {
+        Get-Content $filename |
+            mysql --user=root --password=$pwd --database=$database --comments;
+    }
+    Else {
+        Get-Content $filename |
+            mysql --user=root --host=$server --password=$pwd --database=$database --comments;
+    }
 
     Write-Output $filename;
 }
