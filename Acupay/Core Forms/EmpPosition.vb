a3e9455221a6c00bb852c142e84dadb7e105760c
Lambert Galit <lbg.globagility@gmail.com>
2017-10-23 18:06:23 +0800

    system user positions can now appear in employee positions

--- Acupay/Core Forms/EmpPosition.vb
+++ Acupay/Core Forms/EmpPosition.vb
@@ -195,8 +195,7 @@ Public Class EmpPosition
                                     ",d.RowID AS DivRowID" &
                                     " FROM position p" &
                                     " LEFT JOIN `division` d ON d.RowID=p.DivisionId" &
-                                    " WHERE p.OrganizationID=" & orgztnID & "" &
-                                    " AND p.RowID NOT IN (SELECT PositionID FROM `user` WHERE OrganizationID=" & orgztnID & ");").ResultTable
+                                    " WHERE p.OrganizationID=" & orgztnID & ";").ResultTable
 
         'alphaposition = New SQLQueryToDatatable("SELECT * FROM position WHERE OrganizationID=" & orgztnID & " AND ParentPositionID IS NOT NULL AND ParentPositionID!=RowID GROUP BY ParentPositionID;")
 
@@ -205,8 +204,7 @@ Public Class EmpPosition
                                                 " FROM position p" &
                                                 " INNER JOIN `division` d ON d.RowID=p.DivisionId" &
                                                 " WHERE p.OrganizationID=" & orgztnID & "" &
-                                                " AND p.ParentPositionID IS NULL" &
-                                                " AND p.RowID NOT IN (SELECT PositionID FROM `user` WHERE OrganizationID=" & orgztnID & ");").ResultTable
+                                                " AND p.ParentPositionID IS NULL;").ResultTable
 
         'For Each drow As DataRow In alphaposition.Rows
         '    Positiontreeviewfiller(drow("RowID"), drow("PositionName"), )
