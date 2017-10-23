4ec536ed07da652d89c3ad775cd3df1b27dd4d2c
Veronica Bea Sebastian <sebastianveronicabea@gmail.com>
2017-10-23 17:59:41 +0800

    Added Entity
    Paystub, Item, and Product (October 23, 2017)

---
+++ Acupay/Core Forms/PayStub/Entity/Item.vb
@@ -0,0 +1,34 @@
+﻿Option Strict On
+
+Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace Global.Item.Entity
+
+    <Table("paystubitem")>
+    Public Class Item
+
+        <Key>
+        Public Property RowID As Integer?
+
+        Public Property OrganizationID As Integer?
+
+        Public Property Created As Date
+
+        Public Property CreatedBy As Integer?
+
+        Public Property LastUpd As Date
+
+        Public Property LastUpdBy As Integer
+
+        Public Property PayStubID As Integer?
+
+        Public Property ProductID As Integer?
+
+        Public Property PayAmount As Decimal
+
+        Public Property Undeclared As Char
+
+    End Class
+
+End Namespace
---
+++ Acupay/Core Forms/PayStub/Entity/Paystub.vb
@@ -0,0 +1,104 @@
+﻿Option Strict On
+
+Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace Global.Paystub.Entity
+
+    <Table("paystub")>
+    Public Class Paystub
+
+        <Key>
+        Public Property RowID As Integer?
+
+        Public Property OrganizationID As Integer?
+
+        Public Property Created As Date
+
+        Public Property CreatedBy As Integer?
+
+        Public Property LastUpd As Date
+
+        Public Property LastUpdBy As Integer?
+
+        Public Property PayPeriodID As Integer?
+
+        Public Property EmployeeID As Integer?
+
+        Public Property TimeEntryID As Integer?
+
+        Public Property PayFromdate As Date
+
+        Public Property PayToDate As Date
+
+        Public Property RegularHours As Decimal
+
+        Public Property RegularPay As Decimal
+
+        Public Property OvertimeHours As Decimal
+
+        Public Property OvertimePay As Decimal
+
+        Public Property NightDiffHours As Decimal
+
+        Public Property NightDiffPay As Decimal
+
+        Public Property NightDiffOvertimeHours As Decimal
+
+        Public Property NightDiffOvertimePay As Decimal
+
+        Public Property RestDayHours As Decimal
+
+        Public Property RestDaypay As Decimal
+
+        Public Property LeavePay As Decimal
+
+        Public Property HolidayPay As Decimal
+
+        Public Property LateHours As Decimal
+
+        Public Property LateDeduction As Decimal
+
+        Public Property UndertimeHours As Decimal
+
+        Public Property UndertimeDeduction As Decimal
+
+        Public Property AbsenceDeduction As Decimal
+
+        Public Property WorkPay As Decimal
+
+        Public Property TotalGrossSalary As Decimal
+
+        Public Property TotalNetSalary As Decimal
+
+        Public Property TotalTaxableSalary As Decimal
+
+        Public Property TotalEmpSSS As Decimal
+
+        Public Property TotalEmpWithholdingTax As Decimal
+
+        Public Property TotalCompPhilhealth As Decimal
+
+        Public Property TotalEmpHDMF As Decimal
+
+        Public Property TotalCompHDMF As Decimal
+
+        Public Property TotalVacationDaysLeft As Decimal
+
+        Public Property TotalUndeclaredSalary As Decimal
+
+        Public Property TotalLoans As Decimal
+
+        Public Property TotalBonus As Decimal
+
+        Public Property TotalAllowance As Decimal
+
+        Public Property TotalAdjustments As Decimal
+
+        Public Property ThirteenthMonthInclusion As Boolean
+
+        Public Property FirstTimeSalary As Boolean
+
+    End Class
+
+End Namespace
