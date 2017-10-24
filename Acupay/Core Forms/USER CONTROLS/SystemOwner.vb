8f8d4d95ea833017508716438a3cc7217486e476
Lambert Galit <lbg.globagility@gmail.com>
2017-10-24 08:57:52 +0800

    adjust graphical interface base on current system owner - employee shift (part 3)

--- Acupay/Core Forms/USER CONTROLS/SystemOwner.vb
+++ Acupay/Core Forms/USER CONTROLS/SystemOwner.vb
@@ -7,6 +7,8 @@ Public Class SystemOwner
 
     Const text_cinema2000 As String = "Cinema 2000"
 
+    Const text_default As String = "Default"
+
     Dim sql As New SQL("SELECT Name FROM systemowner WHERE IsCurrentOwner='1' LIMIT 1;")
 
     Private _currentSystemOwner As String = Convert.ToString(sql.GetFoundRow)
@@ -26,6 +28,12 @@ Public Class SystemOwner
     Public Shared ReadOnly Property Cinema2000 As String
         Get
             Return text_cinema2000
+        End Get
+    End Property
+
+    Public Shared ReadOnly Property DefaultOwner As String
+        Get
+            Return text_default
         End Get
     End Property
 
