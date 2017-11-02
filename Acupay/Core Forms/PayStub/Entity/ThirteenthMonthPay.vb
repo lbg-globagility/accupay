863d4ecfce60cd79b05b9bc992e6500e1c941be0
Veronica Bea Sebastian <sebastianveronicabea@gmail.com>
2017-11-02 10:05:36 +0800

    November 2, 2017
    Added entities (Adjustment, Allowance, and Thirteenth Month Pay)

---
+++ Acupay/Core Forms/PayStub/Entity/ThirteenthMonthPay.vb
@@ -0,0 +1,30 @@
+﻿Option Strict On
+
+Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+Namespace Global.AccuPay.Entity
+
+    <Table("thirteenthmonthpay")>
+    Public Class ThirteenthMonthPay
+
+        <Key>
+        Public Property RowID As Integer?
+
+        Public Property OrganizationID As Integer?
+
+        Public Property Created As Date
+
+        Public Property CreatedBy As Integer?
+
+        Public Property LastUpd As Date
+
+        Public Property LastUpdBy As Integer?
+
+        Public Property PaystubID As Integer?
+
+        Public Property Amount As Decimal
+
+    End Class
+
+End Namespace
