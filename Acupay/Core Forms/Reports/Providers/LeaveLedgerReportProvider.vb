4fddadaef86a449fbc5e9911fb20cce7d88ae988
Aaron del Rosario <adelrosario.globagility@gmail.com>
2017-12-14 16:02:01 +0800

    Create stubs for leave ledger report

--- Acupay/Core Forms/Reports/Providers/LeaveLedgerReportProvider.vb
+++ Acupay/Core Forms/Reports/Providers/LeaveLedgerReportProvider.vb
@@ -1,4 +1,6 @@
-﻿Imports Acupay
+﻿Option Strict On
+
+Imports CrystalDecisions.CrystalReports.Engine
 
 Public Class LeaveLedgerReportProvider
     Implements IReportProvider
@@ -6,7 +8,34 @@ Public Class LeaveLedgerReportProvider
     Public Property Name As String = "Leave Ledger" Implements IReportProvider.Name
 
     Public Sub Run() Implements IReportProvider.Run
-        Throw New NotImplementedException()
+        Dim dateSelector As New PayrollSummaDateSelection()
+
+        If Not dateSelector.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Return
+        End If
+
+        Dim dateFrom = dateSelector.DateFrom
+        Dim dateTo = dateSelector.DateTo
+
+        Dim params = New Object(,) {
+            {"OrganizationID", orgztnID},
+            {"PayDateFrom", dateSelector.DateFrom},
+            {"PayDateTo", dateSelector.DateTo}
+        }
+
+        Dim data = DirectCast(callProcAsDatTab(params, "RPT_FiledLeave"), DataTable)
+
+        Dim report = New Employee_Leave_Ledger()
+        report.SetDataSource(data)
+
+        Dim title = DirectCast(report.ReportDefinition.Sections(1).ReportObjects("txtCutoffDate"), TextObject)
+        Dim dateFromTitle = dateFrom?.ToString("MMM dd, yyyy")
+        Dim dateToTitle = dateTo?.ToString("MMM dd, yyyy")
+        title.Text = $"From {dateFromTitle} to {dateToTitle}"
+
+        Dim viewer As New CrysRepForm()
+        viewer.crysrepvwr.ReportSource = report
+        viewer.Show()
     End Sub
 
 End Class
