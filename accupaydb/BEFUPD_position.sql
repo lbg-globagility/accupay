0d4a35f758c14e732f2769db7291a29fb6e20304
Lambert <lbg.globagility@gmail.com>
2017-12-20 15:22:57 +0800

    all position without DivisionID be group into Default Division

--- accupaydb/BEFUPD_position.sql
+++ accupaydb/BEFUPD_position.sql
@@ -9,10 +9,21 @@ SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTIT
 DELIMITER //
 CREATE TRIGGER `BEFUPD_position` BEFORE UPDATE ON `position` FOR EACH ROW BEGIN
 
+IF NEW.DivisionId IS NULL THEN
+
+    INSERT INTO `division`(Name,TradeName,OrganizationID,MainPhone,FaxNumber,BusinessAddress,ContactName,EmailAddress,AltEmailAddress,AltPhone,URL,TINNo,Created,CreatedBy,DivisionType,GracePeriod,WorkDaysPerYear,PhHealthDeductSched,HDMFDeductSched,SSSDeductSched,WTaxDeductSched,DefaultVacationLeave,DefaultSickLeave,DefaultMaternityLeave,DefaultPaternityLeave,DefaultOtherLeave,PayFrequencyID,PhHealthDeductSchedAgency,HDMFDeductSchedAgency,SSSDeductSchedAgency,WTaxDeductSchedAgency,DivisionUniqueID,ParentDivisionID) SELECT 'Default Division', '', NEW.OrganizationID, '', '', '', '', '', '', '', '', '', CURRENT_TIMESTAMP(), NEW.CreatedBy, 'Department', 15.00, 313, 'Per pay period', 'Per pay period', 'Per pay period', 'Per pay period', 40.00, 40.00, 40.00, 40.00, 40.00, 1, 'Per pay period', 'Per pay period', 'Per pay period', 'Per pay period',2,d.RowID FROM division d WHERE d.OrganizationID=NEW.OrganizationID AND d.ParentDivisionID IS NOT NULL LIMIT 1 ON DUPLICATE KEY UPDATE LastUpd=CURRENT_TIMESTAMP(), LastUpdBy=NEW.CreatedBy; SELECT @@identity INTO @primkey_id;
+
+    IF IFNULL(@primkey_id,0) = 0 THEN
+        SET @primkey_id = (SELECT RowID FROM division d WHERE d.Name='Default Division' AND d.OrganizationID=NEW.OrganizationID AND d.ParentDivisionID IS NOT NULL LIMIT 1);
+    END IF;
+
+    SET NEW.DivisionId = @primkey_id;
+END IF;
+
 SELECT EXISTS(SELECT RowID FROM `user` WHERE PositionID=NEW.RowID LIMIT 1)
 INTO @is_sysuser;
 
-IF NEW.DivisionId IS NULL AND @is_sysuser = 0 THEN
+/*IF NEW.DivisionId IS NULL AND @is_sysuser = 0 THEN
     SET NEW.DivisionId = (SELECT RowID FROM division d WHERE d.OrganizationID=NEW.OrganizationID AND d.ParentDivisionID IS NOT NULL LIMIT 1);
 END IF;
 
@@ -21,7 +32,7 @@ INTO @invalid_divisionid;
 
 IF @invalid_divisionid = 1 THEN
     SET NEW.DivisionId = (SELECT RowID FROM division d WHERE d.OrganizationID=NEW.OrganizationID AND d.ParentDivisionID IS NOT NULL LIMIT 1);
-END IF;
+END IF;*/
 
 END//
 DELIMITER ;
