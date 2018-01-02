6acb7ba720d29abc7be5b463fc955b90f8fe0275
Lambert <lbg.globagility@gmail.com>
2018-01-02 16:49:39 +0800

    system assigns initial time range for break hours

---
+++ accupaydb/BEFINS_shift.sql
@@ -0,0 +1,54 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+DROP TRIGGER IF EXISTS `BEFINS_shift`;
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `BEFINS_shift` BEFORE INSERT ON `shift` FOR EACH ROW BEGIN
+
+DECLARE default_break_hour INT(11) DEFAULT 1;
+
+DECLARE divisor_to_half INT(11) DEFAULT 2;
+
+DECLARE sec_per_hour INT(11) DEFAULT 3600;
+
+DECLARE shift_timestamp_from
+        , sh_timestamp_from DATETIME;
+
+DECLARE shift_total_hours INT(11);
+
+DECLARE is_even BOOL DEFAULT FALSE;
+
+SET shift_timestamp_from = CONCAT_DATETIME(CURDATE(), NEW.TimeFrom);
+
+SET shift_total_hours =
+TIMESTAMPDIFF(SECOND
+              , shift_timestamp_from
+				  , CONCAT_DATETIME(ADDDATE(CURDATE(), INTERVAL IS_TIMERANGE_REACHTOMORROW(NEW.TimeFrom, NEW.TimeTo) DAY), NEW.TimeTo)) / sec_per_hour;
+
+SET shift_total_hours = (shift_total_hours / divisor_to_half);
+
+SET is_even = ( (shift_total_hours MOD 2) = 0 );
+
+IF is_even THEN
+
+	SET sh_timestamp_from = ADDDATE(shift_timestamp_from, INTERVAL shift_total_hours HOUR);
+ELSE
+
+	SET sh_timestamp_from = ADDDATE(shift_timestamp_from, INTERVAL (shift_total_hours - default_break_hour) HOUR);
+END IF;
+
+SET NEW.BreakTimeFrom = TIME(sh_timestamp_from);
+
+SET NEW.BreakTimeTo = TIME( ADDDATE(sh_timestamp_from, INTERVAL default_break_hour HOUR) );
+
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
