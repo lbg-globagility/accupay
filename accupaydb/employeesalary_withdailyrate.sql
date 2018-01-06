a884517b3060dde7e8410f9cbf5d5ea90c5d05c3
Lambert <lbg.globagility@gmail.com>
2018-01-06 13:12:30 +0800

    correct the script to import employeesalary_withdailyrate view properly

--- accupaydb/employeesalary_withdailyrate.sql
+++ accupaydb/employeesalary_withdailyrate.sql
@@ -5,36 +5,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 DROP VIEW IF EXISTS `employeesalary_withdailyrate`;
-CREATE TABLE `employeesalary_withdailyrate` (
-	`RowID` INT(11) NOT NULL,
-	`EmployeeID` INT(11) NOT NULL,
-	`Created` DATETIME NOT NULL,
-	`CreatedBy` INT(11) NOT NULL,
-	`LastUpd` DATETIME NULL,
-	`LastUpdBy` INT(11) NULL,
-	`OrganizationID` INT(11) NOT NULL,
-	`FilingStatusID` INT(11) NOT NULL,
-	`PaySocialSecurityID` INT(11) NULL,
-	`PayPhilhealthID` INT(11) NULL,
-	`PhilHealthDeduction` DECIMAL(15,4) NULL,
-	`HDMFAmount` DECIMAL(11,2) NULL,
-	`TrueSalary` DECIMAL(11,2) NULL,
-	`BasicPay` DECIMAL(11,2) NULL,
-	`Salary` DECIMAL(11,2) NULL,
-	`UndeclaredSalary` DECIMAL(11,2) NULL,
-	`BasicDailyPay` DECIMAL(11,2) NULL,
-	`BasicHourlyPay` DECIMAL(11,2) NULL,
-	`NoofDependents` INT(11) NULL,
-	`MaritalStatus` VARCHAR(50) NULL COLLATE 'latin1_swedish_ci',
-	`PositionID` INT(11) NULL,
-	`EffectiveDateFrom` DATE NULL,
-	`EffectiveDateTo` DATE NULL,
-	`OverrideDiscardSSSContrib` TINYINT(4) NULL,
-	`OverrideDiscardPhilHealthContrib` TINYINT(4) NULL,
-	`DailyRate` DECIMAL(19,6) NULL
-) ENGINE=MyISAM;
-
-DROP VIEW IF EXISTS `employeesalary_withdailyrate`;
 DROP TABLE IF EXISTS `employeesalary_withdailyrate`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `employeesalary_withdailyrate` AS SELECT esa.*
 	,(esa.Salary
