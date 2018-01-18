bd8b4030047975a7091d6915ae82ccdc49b4c561
Aaron del Rosario <adelrosario.globagility@gmail.com>
2018-01-18 14:21:30 +0800

    Remove code potentially causing job positions go crazy

--- accupaydb/MASS_generate_employeetimeentry.sql
+++ accupaydb/MASS_generate_employeetimeentry.sql
@@ -10,45 +10,66 @@ CREATE DEFINER=`root`@`127.0.0.1` PROCEDURE `MASS_generate_employeetimeentry`(IN
     DETERMINISTIC
 BEGIN
 
-UPDATE `position` p
-INNER JOIN (SELECT * FROM `user` GROUP BY PositionID) u ON u.RowID > 0
-INNER JOIN `position` pos ON pos.RowID=u.PositionID AND LCASE(pos.PositionName)=LCASE(p.PositionName)
-SET p.DivisionId    =NULL
-        ,p.LastUpd  =IFNULL(ADDTIME(p.LastUpd, SEC_TO_TIME(1)), CURRENT_TIMESTAMP())
-        ,p.LastUpdBy=IFNULL(p.LastUpdBy,p.CreatedBy)
-WHERE p.OrganizationID=OrganizID;
-
-INSERT INTO `position` (`PositionName`, `LastUpd`, `Created`, `CreatedBy`, `OrganizationID`, `LastUpdBy`, `ParentPositionID`, `DivisionId`, `LevelNumber`) SELECT 'Default Position', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), UserRowID, OrganizID, UserRowID, NULL, d.RowID, 3 FROM division d WHERE d.OrganizationID=OrganizID AND ParentDivisionID IS NOT NULL LIMIT 1 ON DUPLICATE KEY UPDATE LastUpd=CURRENT_TIMESTAMP(),LastUpdBy=UserRowID;
+INSERT INTO `position` (
+    `PositionName`,
+    `LastUpd`,
+    `Created`,
+    `CreatedBy`,
+    `OrganizationID`,
+    `LastUpdBy`,
+    `ParentPositionID`,
+    `DivisionId`,
+    `LevelNumber`
+)
+SELECT
+    'Default Position',
+    CURRENT_TIMESTAMP(),
+    CURRENT_TIMESTAMP(),
+    UserRowID,
+    OrganizID,
+    UserRowID,
+    NULL,
+    d.RowID,
+    3
+FROM division d
+WHERE d.OrganizationID = OrganizID AND
+    ParentDivisionID IS NOT NULL
+LIMIT 1
+ON DUPLICATE KEY
+UPDATE LastUpd = CURRENT_TIMESTAMP(),
+    LastUpdBy = UserRowID;
 
 UPDATE employee e
-INNER JOIN `position` p ON p.PositionName='Default Position' AND p.OrganizationID=e.OrganizationID
-SET e.LastUpdBy=IFNULL(e.LastUpdBy,UserRowID),e.PositionID=p.RowID
-WHERE e.OrganizationID=OrganizID AND e.PositionID IS NULL;
+INNER JOIN `position` p
+ON p.PositionName = 'Default Position' AND
+    p.OrganizationID = e.OrganizationID
+SET e.LastUpdBy = IFNULL(e.LastUpdBy, UserRowID),
+    e.PositionID = p.RowID
+WHERE e.OrganizationID = OrganizID AND
+    e.PositionID IS NULL;
 
-UPDATE employee e
-INNER JOIN `position` pos ON pos.RowID=e.PositionID AND pos.OrganizationID=e.OrganizationID AND pos.DivisionId IS NULL
-INNER JOIN `position` p ON p.PositionName='Default Position' AND p.OrganizationID=e.OrganizationID
-SET e.LastUpdBy=IFNULL(e.LastUpdBy,UserRowID),e.PositionID=p.RowID WHERE e.OrganizationID=OrganizID;
-
-UPDATE employee e
-INNER JOIN `position` p ON p.PositionName='Default Position' AND p.OrganizationID=e.OrganizationID
-INNER JOIN (SELECT * FROM `user` GROUP BY PositionID) u ON u.RowID > 0
-INNER JOIN `position` pos ON pos.RowID=u.PositionID
-INNER JOIN `position` pstn ON pstn.RowID=e.PositionID AND LCASE(pstn.PositionName)=LCASE(pos.PositionName)
-SET e.LastUpdBy=IFNULL(e.LastUpdBy,UserRowID),e.PositionID=p.RowID
-WHERE e.OrganizationID=OrganizID;
-
-UPDATE employee SET DayOfRest='1' WHERE OrganizationID=OrganizID AND DayOfRest IN ('','0');
+UPDATE employee
+SET DayOfRest = NULL
+WHERE OrganizationID = OrganizID AND
+    DayOfRest IN ('', '0');
 
 UPDATE employeeovertime eot
-INNER JOIN payfrequency pf ON pf.PayFrequencyType=Pay_FrequencyType
-INNER JOIN `position` ps ON ps.OrganizationID=eot.OrganizationID AND LOCATE(ps.DivisionId,DivisionRowID) > 0
-INNER JOIN employee e ON e.RowID=eot.EmployeeID AND e.OrganizationID=eot.OrganizationID AND e.PayFrequencyID=pf.RowID AND e.PositionID=ps.RowID
+INNER JOIN payfrequency pf
+ON pf.PayFrequencyType = Pay_FrequencyType
+INNER JOIN `position` ps
+ON ps.OrganizationID = eot.OrganizationID AND
+    LOCATE(ps.DivisionId, DivisionRowID) > 0
+INNER JOIN employee e
+ON e.RowID = eot.EmployeeID AND
+    e.OrganizationID = eot.OrganizationID AND
+    e.PayFrequencyID = pf.RowID AND
+    e.PositionID = ps.RowID
 SET
-eot.LastUpd = ADDTIME(eot.LastUpd, '00:00:01'), eot.LastUpdBy = IFNULL(eot.LastUpdBy, eot.CreatedBy)
+    eot.LastUpd = ADDTIME(eot.LastUpd, '00:00:01'),
+    eot.LastUpdBy = IFNULL(eot.LastUpdBy, eot.CreatedBy)
 WHERE eot.OrganizationID = OrganizID
-AND (eot.OTStartDate >= periodDateFrom OR eot.OTEndDate >= periodDateFrom)
-AND (eot.OTStartDate <= periodDateTo OR eot.OTEndDate <= periodDateTo);
+    AND (eot.OTStartDate >= periodDateFrom OR eot.OTEndDate >= periodDateFrom)
+    AND (eot.OTStartDate <= periodDateTo OR eot.OTEndDate <= periodDateTo);
 
 IF ISNULL(DivisionRowID) THEN
     SELECT GENERATE_employeetimeentry(e.RowID, OrganizID, d.DateValue, UserRowID)
