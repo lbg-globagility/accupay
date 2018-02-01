c13ca304fd324184f3a070b08d09c79ddacd99b9
Aaron del Rosario <adelrosario.globagility@gmail.com>
2018-02-01 13:10:41 +0800

    Change design of metrologin client

--- AcupayEmployee/General Forms/MetroLogin.Designer.vb
+++ AcupayEmployee/General Forms/MetroLogin.Designer.vb
@@ -1,10 +1,10 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class MetroLogin
     'Inherits System.Windows.Forms.Form
     Inherits MetroFramework.Forms.MetroForm
-    
+
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -21,7 +21,7 @@ Partial Class MetroLogin
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.MetroStyleManager1 = New MetroFramework.Components.MetroStyleManager(Me.components)
@@ -40,42 +40,39 @@ Partial Class MetroLogin
         '
         'lnklblobf
         '
-        Me.lnklblobf.AutoSize = True
-        Me.lnklblobf.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lnklblobf.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblobf.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblobf.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblobf.Location = New System.Drawing.Point(0, 39)
+        Me.lnklblobf.Location = New System.Drawing.Point(24, 96)
         Me.lnklblobf.Name = "lnklblobf"
-        Me.lnklblobf.Size = New System.Drawing.Size(126, 15)
+        Me.lnklblobf.Size = New System.Drawing.Size(184, 24)
         Me.lnklblobf.TabIndex = 22
         Me.lnklblobf.TabStop = True
-        Me.lnklblobf.Text = "O&fficial Business filing"
+        Me.lnklblobf.Text = "New Official Business"
         '
         'lnklblovertime
         '
-        Me.lnklblovertime.AutoSize = True
-        Me.lnklblovertime.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lnklblovertime.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblovertime.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblovertime.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblovertime.Location = New System.Drawing.Point(0, 24)
+        Me.lnklblovertime.Location = New System.Drawing.Point(24, 56)
         Me.lnklblovertime.Name = "lnklblovertime"
-        Me.lnklblovertime.Size = New System.Drawing.Size(85, 15)
+        Me.lnklblovertime.Size = New System.Drawing.Size(184, 24)
         Me.lnklblovertime.TabIndex = 21
         Me.lnklblovertime.TabStop = True
-        Me.lnklblovertime.Text = "Over&time filing"
+        Me.lnklblovertime.Text = "New Overtime"
         '
         'lnklblleave
         '
-        Me.lnklblleave.AutoSize = True
-        Me.lnklblleave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lnklblleave.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lnklblleave.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.lnklblleave.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.lnklblleave.Location = New System.Drawing.Point(0, 9)
+        Me.lnklblleave.Location = New System.Drawing.Point(24, 16)
         Me.lnklblleave.Name = "lnklblleave"
-        Me.lnklblleave.Size = New System.Drawing.Size(69, 15)
+        Me.lnklblleave.Size = New System.Drawing.Size(184, 24)
         Me.lnklblleave.TabIndex = 20
         Me.lnklblleave.TabStop = True
-        Me.lnklblleave.Text = "Lea&ve filing"
+        Me.lnklblleave.Text = "New Leave of Absence"
         '
         'Panel1
         '
@@ -85,18 +82,17 @@ Partial Class MetroLogin
         Me.Panel1.Controls.Add(Me.lnklblovertime)
         Me.Panel1.Location = New System.Drawing.Point(8, 56)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(520, 304)
+        Me.Panel1.Size = New System.Drawing.Size(504, 192)
         Me.Panel1.TabIndex = 23
         '
         'LinkLabel1
         '
-        Me.LinkLabel1.AutoSize = True
-        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
         Me.LinkLabel1.LinkColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.LinkLabel1.Location = New System.Drawing.Point(132, 9)
+        Me.LinkLabel1.Location = New System.Drawing.Point(24, 136)
         Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(79, 15)
+        Me.LinkLabel1.Size = New System.Drawing.Size(184, 24)
         Me.LinkLabel1.TabIndex = 23
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "My time entry"
@@ -105,7 +101,7 @@ Partial Class MetroLogin
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(544, 371)
+        Me.ClientSize = New System.Drawing.Size(520, 256)
         Me.Controls.Add(Me.Panel1)
         Me.KeyPreview = True
         Me.MaximizeBox = False
@@ -117,7 +113,6 @@ Partial Class MetroLogin
         Me.Text = "Welcome"
         CType(Me.MetroStyleManager1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
