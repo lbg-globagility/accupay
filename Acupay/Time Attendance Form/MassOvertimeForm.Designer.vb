977a0f5b05c76d893c5ce9c2e2eaf5005212f019
Aaron del Rosario <adelrosario.globagility@gmail.com>
2018-03-13 16:37:31 +0800

    Init massovertime form

---
+++ Acupay/Time Attendance Form/MassOvertimeForm.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MassOvertimeForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(8, 32)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(192, 544)
+        Me.TreeView1.TabIndex = 0
+        '
+        'MassOvertimeForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(871, 597)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "MassOvertimeForm"
+        Me.Text = "MassOvertimeForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TreeView1 As TreeView
+End Class
