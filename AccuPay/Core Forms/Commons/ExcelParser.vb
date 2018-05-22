40054d9dc20ff22419c3b33e54558f6da3b78364
Aaron del Rosario <adelrosario.globagility@gmail.com>
2018-05-22 17:38:12 +0800

    Add excel parser

---
+++ AccuPay/Core Forms/Commons/ExcelParser.vb
@@ -0,0 +1,90 @@
+﻿Option Strict On
+
+Imports System.IO
+Imports AccuPay.Utils
+Imports OfficeOpenXml
+
+Namespace Global.AccuPay
+
+    Public Class ExcelParser(Of T As {Class, New})
+
+        Private _worksheetName As String
+
+        Public Sub New(worksheetName As String)
+            _worksheetName = worksheetName
+        End Sub
+
+        Public Sub Read(filePath As String)
+            Dim stream = New MemoryStream()
+
+            Using fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+                fileStream.CopyTo(stream)
+            End Using
+
+            Dim salaryRecords As List(Of T) = Nothing
+
+            Using excel = New ExcelPackage(stream)
+                Dim worksheet = excel.Workbook.Worksheets(_worksheetName)
+
+                If worksheet Is Nothing Then
+                    Throw New Exception($"We can't find the worksheet `{_worksheetName}`.")
+                End If
+
+                Dim tprops = GetType(T).GetProperties().ToList()
+
+                Dim groups = worksheet.Cells.
+                    GroupBy(Function(g) g.Start.Row).
+                    ToList()
+
+                Dim colNames = groups.
+                    First().
+                    Select(Function(col, index) New With {.Name = col.Value.ToString(), .Index = index}).
+                    ToList()
+
+                Dim rowValues = groups.
+                    Skip(1).
+                    Select(Function(cfg) cfg.Select(Function(g) g.Value).ToList())
+
+                salaryRecords = rowValues.
+                    Select(
+                        Function(row)
+                            Dim newRecord = New T()
+
+                            colNames.ForEach(
+                                Sub(column)
+                                    If column.Index >= row.Count Then
+                                        Return
+                                    End If
+
+                                    Dim originalValue = row(column.Index)
+                                    Dim prop = tprops.First(
+                                        Function(t)
+                                            Return StringUtils.ToPascal(t.Name) = StringUtils.ToPascal(column.Name)
+                                        End Function)
+
+                                    If TypeOf originalValue Is Double Then
+                                        Dim value = DirectCast(originalValue, Double)
+
+                                        If prop.PropertyType Is GetType(Double) Then
+                                            prop.SetValue(newRecord, value)
+                                        ElseIf prop.PropertyType Is GetType(Decimal) Then
+                                            prop.SetValue(newRecord, CDec(value))
+                                        ElseIf prop.PropertyType Is GetType(Integer) Then
+                                            prop.SetValue(newRecord, CInt(value))
+                                        ElseIf prop.PropertyType Is GetType(Date) Then
+                                            prop.SetValue(newRecord, Date.FromOADate(value))
+                                        End If
+                                    Else
+                                        prop.SetValue(newRecord, originalValue)
+                                    End If
+                                End Sub)
+
+                            Return newRecord
+                        End Function).
+                    ToList()
+            End Using
+        End Sub
+
+    End Class
+
+End Namespace
---
+++ AccuPay/Core Forms/Utils/StringUtils.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Globalization
+Imports System.Text.RegularExpressions
+
+Namespace Global.AccuPay.Utils
+
+    Public Class StringUtils
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function ToPascal(text As String) As String
+            Dim newText = Regex.Replace(text, "([A-Z])", " $1")
+
+            Dim info = CultureInfo.CurrentCulture.TextInfo
+            Return info.ToTitleCase(newText).Replace(" ", String.Empty)
+        End Function
+
+        Public Shared Function Strip(text As String) As String
+            Return text.Replace(" ", String.Empty)
+        End Function
+
+    End Class
+
+End Namespace
