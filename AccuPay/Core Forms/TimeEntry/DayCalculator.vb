2033ca934bf7faf426cb8b03c2ebeb6b194ca011
Aaron <aaron.delrosario09@gmail.com>
2018-08-12 19:29:05 +0800

    feat: add night diff times

--- AccuPay/Core Forms/TimeEntry/DayCalculator.vb
+++ AccuPay/Core Forms/TimeEntry/DayCalculator.vb
@@ -6,27 +6,38 @@ Imports Microsoft.EntityFrameworkCore
 
 Public Class DayCalculator
 
-    Public Sub Compute(employee As Employee, currentDate As DateTime)
+    Public Sub Compute(employee As Employee, currentDate As DateTime, organization As Organization)
+        Dim timeLog As TimeLog = Nothing
+        Dim shift As ShiftSchedule = Nothing
+
         Using context = New PayrollContext()
-            Dim timeLog = context.TimeLogs.
+            timeLog = context.TimeLogs.
                 SingleOrDefault(Function(t) t.LogDate = currentDate)
 
-            Dim shift = context.ShiftSchedules.
+            shift = context.ShiftSchedules.
                 Include(Function(s) s.Shift).
                 Where(Function(s) Nullable.Equals(s.EmployeeID, employee.RowID)).
                 FirstOrDefault(Function(s) s.EffectiveFrom = currentDate)
-
-            Dim nextDay = currentDate.AddDays(1)
-
-            Dim fullTimeIn = Calendar.Create(currentDate, timeLog.TimeIn)
-            Dim fullTimeOut = Calendar.Create(If(timeLog.TimeOut > timeLog.TimeIn, currentDate, nextDay), timeLog.TimeOut)
-            Dim logPeriod = New TimePeriod(fullTimeIn, fullTimeOut)
-
-            Dim currentShift = New CurrentShift(shift.Shift, currentDate)
-
-            Dim shiftPeriod = currentShift.ShiftPeriod
-            Dim dutyPeriod = shiftPeriod.Overlap(logPeriod)
         End Using
+
+        Dim nextDay = currentDate.AddDays(1)
+
+        Dim fullTimeIn = Calendar.Create(currentDate, TimeLog.TimeIn)
+        Dim fullTimeOut = Calendar.Create(If(TimeLog.TimeOut > TimeLog.TimeIn, currentDate, nextDay), TimeLog.TimeOut)
+        Dim logPeriod = New TimePeriod(fullTimeIn, fullTimeOut)
+
+        Dim currentShift = New CurrentShift(Shift.Shift, currentDate)
+
+        Dim shiftPeriod = currentShift.ShiftPeriod
+        Dim dutyPeriod = shiftPeriod.Overlap(logPeriod)
+
+        Dim nightDiffTimeFrom = organization.NightDifferentialTimeFrom
+        Dim nightDiffTimeTo = organization.NightDifferentialTimeTo
+        Dim nightDiffStart = Calendar.Create(currentDate, nightDiffTimeFrom)
+        Dim nightDiffEnd = Calendar.Create(If(nightDiffTimeTo > nightDiffTimeTo, currentDate, nextDay), nightDiffTimeTo)
+        Dim nightDiffPeriod = New TimePeriod(nightDiffStart, nightDiffEnd)
+
+
     End Sub
 
 End Class
