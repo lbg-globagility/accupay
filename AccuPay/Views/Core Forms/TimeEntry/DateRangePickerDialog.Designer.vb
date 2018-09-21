8aa045ae89de715f5a2ebd8a57ab04aaf177d9fc
aaron_delrosario <adelrosario.globagility@gmail.com>
2018-09-21 16:56:46 +0800

    feat: add payperiods to date range picker

--- AccuPay/Views/Core Forms/TimeEntry/DateRangePickerDialog.Designer.vb
+++ AccuPay/Views/Core Forms/TimeEntry/DateRangePickerDialog.Designer.vb
@@ -26,29 +26,35 @@ Partial Class DateRangePickerDialog
         Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.PayperiodsDataGridView = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DataGridViewTextBoxColumn2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.PayperiodsDataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'DateTimePicker1
         '
         Me.DateTimePicker1.CustomFormat = "MMM dd, yyyy"
         Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker1.Location = New System.Drawing.Point(88, 64)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(224, 224)
         Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.Size = New System.Drawing.Size(208, 20)
         Me.DateTimePicker1.TabIndex = 0
         '
         'DateTimePicker2
         '
         Me.DateTimePicker2.CustomFormat = "MMM dd, yyyy"
         Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
-        Me.DateTimePicker2.Location = New System.Drawing.Point(88, 112)
+        Me.DateTimePicker2.Location = New System.Drawing.Point(8, 224)
         Me.DateTimePicker2.Name = "DateTimePicker2"
-        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker2.Size = New System.Drawing.Size(208, 20)
         Me.DateTimePicker2.TabIndex = 1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(160, 224)
+        Me.Button1.Location = New System.Drawing.Point(280, 256)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 2
@@ -57,24 +63,68 @@ Partial Class DateRangePickerDialog
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(256, 224)
+        Me.Button2.Location = New System.Drawing.Point(360, 256)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "Cancel"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'PayperiodsDataGridView
+        '
+        Me.PayperiodsDataGridView.AllowUserToAddRows = False
+        Me.PayperiodsDataGridView.AllowUserToDeleteRows = False
+        Me.PayperiodsDataGridView.AllowUserToResizeColumns = False
+        Me.PayperiodsDataGridView.AllowUserToResizeRows = False
+        Me.PayperiodsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.PayperiodsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.PayperiodsDataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2})
+        Me.PayperiodsDataGridView.Location = New System.Drawing.Point(8, 32)
+        Me.PayperiodsDataGridView.MultiSelect = False
+        Me.PayperiodsDataGridView.Name = "PayperiodsDataGridView"
+        Me.PayperiodsDataGridView.ReadOnly = True
+        Me.PayperiodsDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.PayperiodsDataGridView.Size = New System.Drawing.Size(424, 184)
+        Me.PayperiodsDataGridView.TabIndex = 4
+        '
+        'Column1
+        '
+        Me.Column1.DataPropertyName = "PayFromDate"
+        Me.Column1.HeaderText = "Start"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.DataPropertyName = "PayToDate"
+        Me.Column2.HeaderText = "End"
+        Me.Column2.Name = "Column2"
+        '
+        'DataGridViewTextBoxColumn1
+        '
+        Me.DataGridViewTextBoxColumn1.HeaderText = "Start"
+        Me.DataGridViewTextBoxColumn1.Name = "DataGridViewTextBoxColumn1"
+        Me.DataGridViewTextBoxColumn1.Width = 191
+        '
+        'DataGridViewTextBoxColumn2
+        '
+        Me.DataGridViewTextBoxColumn2.HeaderText = "End"
+        Me.DataGridViewTextBoxColumn2.Name = "DataGridViewTextBoxColumn2"
+        Me.DataGridViewTextBoxColumn2.Width = 190
+        '
         'DateRangePickerDialog
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(369, 273)
+        Me.ClientSize = New System.Drawing.Size(440, 289)
+        Me.Controls.Add(Me.PayperiodsDataGridView)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DateTimePicker2)
         Me.Controls.Add(Me.DateTimePicker1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Name = "DateRangePickerDialog"
         Me.Text = "DateRangePickerDialog"
+        CType(Me.PayperiodsDataGridView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -83,4 +133,9 @@ Partial Class DateRangePickerDialog
     Friend WithEvents DateTimePicker2 As DateTimePicker
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents PayperiodsDataGridView As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn1 As DataGridViewTextBoxColumn
+    Friend WithEvents DataGridViewTextBoxColumn2 As DataGridViewTextBoxColumn
 End Class
--- AccuPay/Views/Core Forms/TimeEntry/DateRangePickerDialog.vb
+++ AccuPay/Views/Core Forms/TimeEntry/DateRangePickerDialog.vb
@@ -1,6 +1,17 @@
 ﻿Option Strict On
 
+Imports AccuPay.Entity
+Imports Microsoft.EntityFrameworkCore
+
 Public Class DateRangePickerDialog
+
+    Private _payFrequencyId As Integer = 1
+
+    Private _currentPayperiod As PayPeriod
+
+    Private _payperiods As IList(Of PayPeriod)
+
+    Private _year As Integer = 2018
 
     Private _start As Date
 
@@ -17,6 +28,29 @@ Public Class DateRangePickerDialog
             Return _end
         End Get
     End Property
+
+    Private Async Sub DateRangePickerDialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PayperiodsDataGridView.AutoGenerateColumns = False
+
+        Using context = New PayrollContext()
+            _payperiods = Await context.PayPeriods.
+                Where(Function(p) p.Year = _year).
+                Where(Function(p) Nullable.Equals(p.OrganizationID, z_OrganizationID)).
+                Where(Function(p) Nullable.Equals(p.PayFrequencyID, _payFrequencyId)).
+                ToListAsync()
+        End Using
+
+        PayperiodsDataGridView.DataSource = _payperiods
+    End Sub
+
+    Private Sub PayperiodsDataGridView_SelectionChanged(sender As Object, e As EventArgs) Handles PayperiodsDataGridView.SelectionChanged
+        Dim payperiod = DirectCast(PayperiodsDataGridView.CurrentRow.DataBoundItem, PayPeriod)
+
+        _currentPayperiod = payperiod
+
+        _start = payperiod.PayFromDate
+        _end = payperiod.PayToDate
+    End Sub
 
     Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
         _start = DateTimePicker1.Value.Date
