fc0f6b7f4fc65a0de20d81d00496728cc2a6c9d6
lambertgalit092 <lbg.globagility@gmail.com>
2019-01-03 15:09:43 +0800

    loan ledger report

---
+++ AccuPay/Reports/Reports/Providers/LoanLedgerReportProvider.vb
@@ -0,0 +1,60 @@
+﻿Option Strict On
+
+Imports CrystalDecisions.CrystalReports.Engine
+
+Public Class LoanLedgerReportProvider
+    Implements IReportProvider
+
+    Public Property Name As String = "Loan Ledger" Implements IReportProvider.Name
+
+    Private orgNameAddressSql As String =
+        "SELECT
+        CONCAT_WS('\n'
+        , UCASE(og.`Name`)
+        , CONCAT_WS(', '
+                , a.StreetAddress1
+                , a.StreetAddress2
+                , a.Barangay
+                , a.CityTown
+                , a.Country
+                , a.State
+                , a.ZipCode)
+		        )
+        `FullAddress`
+        FROM organization og
+        LEFT JOIN (SELECT RowID
+			        , IF(LENGTH(TRIM(StreetAddress1)) = 0, NULL, TRIM(StreetAddress1)) `StreetAddress1`
+			        , IF(LENGTH(TRIM(StreetAddress2)) = 0, NULL, TRIM(StreetAddress2)) `StreetAddress2`
+			        , IF(LENGTH(TRIM(Barangay)) = 0, NULL, TRIM(Barangay)) `Barangay`
+			        , IF(LENGTH(TRIM(CityTown)) = 0, NULL, TRIM(CityTown)) `CityTown`
+			        , IF(LENGTH(TRIM(Country)) = 0, NULL, TRIM(Country)) `Country`
+			        , IF(LENGTH(TRIM(State)) = 0, NULL, TRIM(State)) `State`
+			        , IF(LENGTH(TRIM(ZipCode)) = 0, NULL, TRIM(ZipCode)) `ZipCode`
+
+			        FROM address) a ON a.RowID=og.PrimaryAddressID
+        WHERE og.RowID = ?ogId;"
+
+    Public Sub Run() Implements IReportProvider.Run
+
+        Dim data = New SQL("CALL RPT_LoanLedger();").GetFoundRows.Tables.OfType(Of DataTable).FirstOrDefault
+
+        Dim report = New LoanLedgerReport()
+        report.SetDataSource(data)
+
+        Dim sql As New SQL(orgNameAddressSql,
+                           New Object() {2}) 'orgztnID
+        Dim result = Convert.ToString(sql.GetFoundRow)
+        Dim splitValues = Split(result, Chr(10))
+
+        Dim txtCompanyName = DirectCast(report.ReportDefinition.Sections(1).ReportObjects("txtOrgName"), TextObject)
+        txtCompanyName.Text = splitValues.First
+
+        Dim txtAddress = DirectCast(report.ReportDefinition.Sections(1).ReportObjects("txtAddress"), TextObject)
+        txtAddress.Text = splitValues.Last
+
+        Dim viewer As New CrysRepForm()
+        viewer.crysrepvwr.ReportSource = report
+        viewer.Show()
+    End Sub
+
+End Class
