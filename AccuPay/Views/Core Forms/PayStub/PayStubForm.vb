a162efc38bfe36962307a55770f8857118589a5b
Joshua Santos <jsantos.globagility@gmail.com>
2019-02-08 15:24:39 +0800

    Fix the issue on new employees' paystub not showing their salary properly because of FirstTimeSalary

--- AccuPay/Views/Core Forms/PayStub/PayStubForm.vb
+++ AccuPay/Views/Core Forms/PayStub/PayStubForm.vb
@@ -1426,7 +1426,7 @@ Public Class PayStubForm
                 Dim basicPay = ValNoComma(drow("BasicPay"))
                 Dim deductions = 0.0
 
-                If drow("FirstTimeSalary").ToString = "1" Then
+                If drow("FirstTimeSalary").ToString = "True" Then
                     basicPay = ValNoComma(drow("RegularPay"))
                 Else
                     basicPay = ValNoComma(drow("BasicPay"))
@@ -1468,7 +1468,7 @@ Public Class PayStubForm
                 Dim thebasicpay = ValNoComma(drow("BasicPay"))
                 Dim thelessamounts = ValNoComma(0)
 
-                If drow("FirstTimeSalary").ToString = "1" Then
+                If drow("FirstTimeSalary").ToString = "True" Then
                     thebasicpay = ValNoComma(drow("RegularPay"))
                     lblSubtotal.Text = FormatNumber(ValNoComma(drow("TotalDayPay")), 2)
                 Else
@@ -1608,7 +1608,7 @@ Public Class PayStubForm
                 Dim thebasicpay = ValNoComma(drow("BasicPay"))
                 Dim thelessamounts = ValNoComma(0)
 
-                If drow("FirstTimeSalary").ToString = "1" Then
+                If drow("FirstTimeSalary").ToString = "True" Then
                     thebasicpay = ValNoComma(drow("RegularHoursAmount"))
                 Else
                     thebasicpay = ValNoComma(drow("BasicPay"))
@@ -1649,7 +1649,7 @@ Public Class PayStubForm
                 Dim thebasicpay = ValNoComma(drow("BasicPay"))
                 Dim thelessamounts = ValNoComma(0)
 
-                If drow("FirstTimeSalary").ToString = "1" Then
+                If drow("FirstTimeSalary").ToString = "True" Then
                     thebasicpay = ValNoComma(drow("RegularHoursAmount"))
                     lblSubtotal.Text = FormatNumber(ValNoComma(drow("TotalDayPay")), 2)
                 Else
