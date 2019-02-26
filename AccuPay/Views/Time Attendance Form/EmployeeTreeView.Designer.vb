f93a78bcf1a7e10f0943ae17312be9ab97aae7f3
Lambert Galit <lbg.globagility@gmail.com>
2019-02-26 14:04:29 +0800

    create a employee treeview UserControl

---
+++ AccuPay/Views/Time Attendance Form/EmployeeTreeView.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EmployeeTreeView
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.AccuPayEmployeeTreeView = New AccuPay.AccuPayTreeView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'AccuPayEmployeeTreeView
+        '
+        Me.AccuPayEmployeeTreeView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.AccuPayEmployeeTreeView.CheckBoxes = True
+        Me.AccuPayEmployeeTreeView.Location = New System.Drawing.Point(0, 26)
+        Me.AccuPayEmployeeTreeView.Name = "AccuPayEmployeeTreeView"
+        Me.AccuPayEmployeeTreeView.Size = New System.Drawing.Size(156, 165)
+        Me.AccuPayEmployeeTreeView.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(156, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(156, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Search"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.AccuPayEmployeeTreeView)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 13)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(156, 191)
+        Me.Panel1.TabIndex = 5
+        '
+        'EmployeeTreeView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "EmployeeTreeView"
+        Me.Size = New System.Drawing.Size(156, 204)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents AccuPayEmployeeTreeView As AccuPayTreeView
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ AccuPay/Views/Time Attendance Form/EmployeeTreeView.vb
@@ -0,0 +1,234 @@
+﻿Option Strict On
+
+Imports AccuPay.Entity
+Imports AccuPay.Tools
+Imports Microsoft.EntityFrameworkCore
+Imports System.Collections.ObjectModel
+Imports System.Data.Entity
+Imports System.Threading.Tasks
+
+Public Class EmployeeTreeView
+
+    Private _presenter As EmployeeTreeViewPresenter
+
+    Public Event OrganizationIDChanged(s As Object, e As EventArgs)
+
+    Public Event FiltersEmployee(s As Object, e As EventArgs)
+
+    Public Property OrganizationID As Integer
+        Get
+
+        End Get
+        Set(value As Integer)
+            WhenOrganizationIdChanged(value)
+        End Set
+    End Property
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+        _presenter = New EmployeeTreeViewPresenter(Me)
+    End Sub
+
+    Private Sub EmployeeTreeView_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If DesignMode Then
+            Return
+        End If
+
+        _presenter.Load()
+        AddHandler AccuPayEmployeeTreeView.AfterCheck, AddressOf EmployeeTreeView_AfterCheck
+    End Sub
+
+    Private Sub WhenOrganizationIdChanged(organizationId As Integer)
+        If DesignMode Then
+            Return
+        End If
+
+        RaiseEvent OrganizationIDChanged(Me, New EventArgs)
+        _presenter.Load(organizationId)
+    End Sub
+
+    Private Sub EmployeeTreeView_AfterCheck(sender As Object, e As TreeViewEventArgs)
+        RemoveHandler AccuPayEmployeeTreeView.AfterCheck, AddressOf EmployeeTreeView_AfterCheck
+        SetCheck(e.Node)
+        SetParent(e.Node)
+        AddHandler AccuPayEmployeeTreeView.AfterCheck, AddressOf EmployeeTreeView_AfterCheck
+    End Sub
+
+    Public Function GetActiveEmployees() As IList(Of Employee)
+        Return _presenter.GetActiveEmployees
+    End Function
+
+    Private Sub SetCheck(node As TreeNode)
+        If node.GetNodeCount(False) >= 1 Then
+            For Each childNode As TreeNode In node.Nodes
+                childNode.Checked = node.Checked
+                SetCheck(childNode)
+            Next
+        End If
+    End Sub
+
+    Private Sub SetParent(node As TreeNode)
+        If node.Parent IsNot Nothing Then
+            Dim areSiblingsSame = True
+            For Each siblingNode As TreeNode In node.Parent.Nodes
+                If siblingNode.Checked <> node.Checked Then
+                    areSiblingsSame = False
+                End If
+            Next
+
+            If areSiblingsSame And node.Checked Then
+                node.Parent.Checked = True
+            Else
+                node.Parent.Checked = False
+            End If
+
+            SetParent(node.Parent)
+        End If
+    End Sub
+
+    Public Sub ShowEmployees(divisions As IEnumerable(Of Division), employees As IEnumerable(Of Employee))
+        AccuPayEmployeeTreeView.BeginUpdate()
+        AccuPayEmployeeTreeView.Nodes.Clear()
+
+        Dim parentDivisions = divisions.Where(Function(d) d.IsRoot)
+
+        For Each parentDivision In parentDivisions
+            Dim parentNode = New TreeNode() With {
+                .Name = parentDivision.Name,
+                .Text = parentDivision.Name,
+                .Tag = parentDivision
+            }
+
+            Dim childDivisions = divisions.Where(Function(d) d.IsParent(parentDivision))
+            For Each childDivision In childDivisions
+                Dim childNode = New TreeNode() With {
+                    .Name = childDivision.Name,
+                    .Text = childDivision.Name,
+                    .Tag = childDivision
+                }
+
+                Dim childEmployees = employees.
+                    Where(Function(e) Nullable.Equals(e.Position?.Division.RowID, childDivision.RowID))
+                For Each childEmployee In childEmployees
+                    Dim employeeNode = New TreeNode() With {
+                        .Name = childEmployee.Fullname,
+                        .Text = childEmployee.Fullname,
+                        .Tag = childEmployee
+                    }
+
+                    childNode.Nodes.Add(employeeNode)
+                Next
+
+                parentNode.Nodes.Add(childNode)
+            Next
+
+            AccuPayEmployeeTreeView.Nodes.Add(parentNode)
+        Next
+
+        AccuPayEmployeeTreeView.ExpandAll()
+        AccuPayEmployeeTreeView.EndUpdate()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        _presenter.FilterEmployees(TextBox1.Text)
+
+        RaiseEvent FiltersEmployee(e, New EventArgs)
+    End Sub
+
+    Private Class EmployeeTreeViewPresenter
+
+        Private _employees As IList(Of Employee)
+
+        Private _divisions As IList(Of Division)
+
+        Private _view As EmployeeTreeView
+
+        Private _models As List(Of OvertimeModel)
+
+        Private _organizationId As Integer
+
+        Public Sub New(view As EmployeeTreeView)
+            _view = view
+            _organizationId = view.OrganizationID
+        End Sub
+
+        Public Sub Load()
+            Reload()
+        End Sub
+
+        Public Sub Load(organizationId As Integer)
+            _organizationId = organizationId
+            Reload()
+        End Sub
+
+        Private Sub Reload()
+            _divisions = LoadDivisions()
+            _employees = LoadEmployees()
+
+            _view.ShowEmployees(_divisions, _employees)
+        End Sub
+
+        Private Function LoadDivisions() As IList(Of Division)
+            Using context = New PayrollContext()
+                Return context.Divisions.
+                    Where(Function(d) Nullable.Equals(d.OrganizationID, _organizationId)).
+                    ToList()
+            End Using
+        End Function
+
+        Private Function LoadEmployees() As IList(Of Employee)
+            Using context = New PayrollContext()
+                Return context.Employees.Include(Function(e) e.Position.Division).
+                    Where(Function(e) Nullable.Equals(e.OrganizationID, _organizationId)).
+                    OrderBy(Function(e) e.LastName).
+                    ThenBy(Function(e) e.FirstName).
+                    ToList()
+            End Using
+        End Function
+
+        Public Async Sub FilterEmployees(needle As String)
+            Dim match =
+                Function(employee As Employee) As Boolean
+                    Dim contains = employee.Fullname.ToLower().Contains(needle)
+                    Dim reverseName = ($"{employee.LastName} {employee.FirstName}").ToLower()
+                    Dim containsReverseName = reverseName.Contains(needle)
+
+                    Return contains Or containsReverseName
+                End Function
+
+            Dim employees = Await Task.Run(
+                Function()
+                    Return _employees.Where(match).ToList()
+                End Function)
+
+            _view.ShowEmployees(_divisions, employees)
+        End Sub
+
+        Public Function GetActiveEmployees() As IList(Of Employee)
+            Dim list = New List(Of Employee)
+            For Each node As TreeNode In _view.AccuPayEmployeeTreeView.Nodes
+                TraverseNodes(node, list)
+            Next
+            Return list
+        End Function
+
+        Private Sub TraverseNodes(node As TreeNode, list As IList(Of Employee))
+            If TypeOf node.Tag Is Employee And node.Checked Then
+                list.Add(DirectCast(node.Tag, Employee))
+            End If
+
+            If node.GetNodeCount(False) >= 1 Then
+                For Each child As TreeNode In node.Nodes
+                    TraverseNodes(child, list)
+                Next
+            End If
+        End Sub
+
+    End Class
+
+End Class
