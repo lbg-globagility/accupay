58b02f55e679a65e205c9cc033314fa0875a4119
Lambert <lbg.globagility@gmail.com>
2019-03-28 09:13:11 +0800

    finishing touch for new time logs

--- AccuPay/Views/Initial Forms/TimeAttendForm.Designer.vb
+++ AccuPay/Views/Initial Forms/TimeAttendForm.Designer.vb
@@ -26,18 +26,18 @@ Partial Class TimeAttendForm
         Me.TimeEntToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ShiftScheduleToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.TimeEntryLogsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.TimeEntryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SummaryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.OvertimeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PanelTimeAttend = New System.Windows.Forms.Panel()
-        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
         Me.MenuStrip1.BackColor = System.Drawing.Color.Transparent
-        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TimeEntToolStripMenuItem, Me.ShiftScheduleToolStripMenuItem, Me.TimeEntryLogsToolStripMenuItem, Me.ToolStripMenuItem1, Me.TimeEntryToolStripMenuItem, Me.SummaryToolStripMenuItem, Me.OvertimeToolStripMenuItem})
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TimeEntToolStripMenuItem, Me.ShiftScheduleToolStripMenuItem, Me.ToolStripMenuItem1, Me.TimeEntryLogsToolStripMenuItem, Me.TimeEntryToolStripMenuItem, Me.SummaryToolStripMenuItem, Me.OvertimeToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(1006, 24)
@@ -61,6 +61,12 @@ Partial Class TimeAttendForm
         Me.TimeEntryLogsToolStripMenuItem.Name = "TimeEntryLogsToolStripMenuItem"
         Me.TimeEntryLogsToolStripMenuItem.Size = New System.Drawing.Size(101, 20)
         Me.TimeEntryLogsToolStripMenuItem.Text = "Time entry logs"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(74, 20)
+        Me.ToolStripMenuItem1.Text = "Time Logs"
         '
         'TimeEntryToolStripMenuItem
         '
@@ -89,12 +95,6 @@ Partial Class TimeAttendForm
         Me.PanelTimeAttend.Name = "PanelTimeAttend"
         Me.PanelTimeAttend.Size = New System.Drawing.Size(1006, 446)
         Me.PanelTimeAttend.TabIndex = 3
-        '
-        'ToolStripMenuItem1
-        '
-        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(101, 20)
-        Me.ToolStripMenuItem1.Text = "Time entry logs"
         '
         'TimeAttendForm
         '
