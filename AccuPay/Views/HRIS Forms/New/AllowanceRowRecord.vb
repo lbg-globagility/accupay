cc5e9cada2d5c335a0171bfd07c291ccb77aa0c5
littleSparrow7 <littleSparrow7@yandex.com>
2019-05-03 15:29:54 +0800

    feat: view import allowance errors and download allowance template

---
+++ AccuPay/Views/HRIS Forms/New/AllowanceRowRecord.vb
@@ -0,0 +1,37 @@
+﻿Option Strict On
+Imports AccuPay.Attributes
+
+Namespace Global.Globagility.AccuPay.Loans
+
+    Public Class AllowanceRowRecord
+
+
+        <ColumnName("EmployeeID")>
+        Public Property EmployeeID As String
+
+        <ColumnName("Name of allowance")>
+        Public Property Type As String
+
+        <ColumnName("Effective start date")>
+        Public Property EffectiveStartDate As Date
+
+        <ColumnName("Effective end date")>
+        Public Property EffectiveEndDate As Date
+
+        <ColumnName("Allowance frequency(Daily, Semi-monthly)")>
+        Public Property AllowanceFrequency As String
+
+        <ColumnName("Allowance amount")>
+        Public Property Amount As Decimal
+
+        Public Property EmployeeFullName As String
+        <Ignore>
+        Public Property ErrorMessage As String
+
+        <Ignore>
+        Public Property LineNumber As Integer
+
+    End Class
+
+End Namespace
+
