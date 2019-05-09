5a02344ba733f186fe89cea6a857f4930a87f641
littleSparrow7 <littleSparrow7@yandex.com>
2019-05-09 16:59:20 +0800

    import employee no and loan type to excel file

--- AccuPay/Core/Utilities/Helper/DownloadTemplateHelper.vb
+++ AccuPay/Core/Utilities/Helper/DownloadTemplateHelper.vb
@@ -1,6 +1,9 @@
 ﻿Option Strict On
 Imports System.IO
+Imports AccuPay.Entity
+Imports AccuPay.Repository
 Imports AccuPay.Utils
+Imports OfficeOpenXml
 
 Namespace Global.AccuPay.Helpers
 
@@ -28,6 +31,50 @@ Namespace Global.AccuPay.Helpers
             End Try
 
         End Sub
+
+
+        Public Shared Async Function DownloadWithData(excelTemplate As ExcelTemplates) As Threading.Tasks.Task(Of FileInfo)
+            Dim _employeeRepository As New EmployeeRepository
+            Dim excelName = TemplatesHelper.GetFileName(excelTemplate)
+            Dim template = TemplatesHelper.GetFullPath(excelTemplate)
+
+            Try
+
+                Dim saveFileDialogHelperOutPut = SaveFileDialogHelper.BrowseFile(excelName)
+
+                If saveFileDialogHelperOutPut.IsSuccess = False Then Return Nothing
+
+                File.Copy(template, saveFileDialogHelperOutPut.FileInfo.FullName)
+
+                Dim fileInfo = saveFileDialogHelperOutPut.FileInfo
+
+                'Import employee numbers
+                Using package As New ExcelPackage(fileInfo)
+                    Dim worksheet As ExcelWorksheet = package.Workbook.Worksheets("Options")
+                    Dim allEmployees = CType(Await _employeeRepository.GetAllAsync(Of Employee), List(Of Employee))
+                    Dim allEmployed = allEmployees.
+                        Where(Function(emp) emp.EmploymentStatus <> "Terminated").
+                        Where(Function(emp) emp.EmploymentStatus <> "Resigned").
+                        Select(Function(emp) emp.EmployeeNo).
+                        ToList()
+
+                    For index = 0 To allEmployed.Count - 1
+                        worksheet.Cells(index + 2, 1).Value = allEmployed(index)
+                    Next
+
+                    package.Save()
+                End Using
+
+                Return fileInfo
+
+            Catch ex As Exception
+
+                MessageBoxHelper.DefaultErrorMessage()
+
+            End Try
+
+            Return Nothing
+        End Function
 
     End Class
 
