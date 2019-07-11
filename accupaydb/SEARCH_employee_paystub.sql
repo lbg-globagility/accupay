e139bddd9a8fc386edf41a96f29be0394cd4a699
Joshua Santos <jsantos.globagility@gmail.com>
2019-07-11 10:26:56 +0800

    Remove the filtering of inactive employees in payroll form.

--- accupaydb/SEARCH_employee_paystub.sql
+++ accupaydb/SEARCH_employee_paystub.sql
@@ -46,8 +46,7 @@ WHERE p.OrganizationID = $organizationId AND
         e.FirstName = $searchTerm OR
         e.LastName = $searchTerm OR
         e.EmployeeID = $searchTerm
-    )) AND
-	FIND_IN_SET(e.EmploymentStatus, UNEMPLOYEMENT_STATUSES()) = 0
+    ))
 ORDER BY e.LastName, e.FirstName
 LIMIT $offset, $limit;
 
