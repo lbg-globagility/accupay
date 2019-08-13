9b77f74751cc9ce9a3ce51698c6b971f57773446
aaron_delrosario <adelrosario.globagility@gmail.com>
2019-08-13 10:49:18 +0800

    fix: add auth requirement for salary tab

--- AccuPay/Views/Core Forms/EmployeeForm.Designer.vb
+++ AccuPay/Views/Core Forms/EmployeeForm.Designer.vb
@@ -1859,7 +1859,7 @@ Partial Class EmployeeForm
         Me.SplitContainer2.Panel2.Controls.Add(Me.lblforballoon1)
         Me.SplitContainer2.Panel2.Cursor = System.Windows.Forms.Cursors.Default
         Me.SplitContainer2.Size = New System.Drawing.Size(854, 455)
-        Me.SplitContainer2.SplitterDistance = 267
+        Me.SplitContainer2.SplitterDistance = 264
         Me.SplitContainer2.SplitterWidth = 7
         Me.SplitContainer2.TabIndex = 114
         '
@@ -3126,7 +3126,7 @@ Partial Class EmployeeForm
         Me.dgvDepen.Name = "dgvDepen"
         Me.dgvDepen.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing
         Me.dgvDepen.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.dgvDepen.Size = New System.Drawing.Size(852, 154)
+        Me.dgvDepen.Size = New System.Drawing.Size(852, 157)
         Me.dgvDepen.TabIndex = 116
         '
         'Colmn0
@@ -9080,6 +9080,7 @@ Partial Class EmployeeForm
         '
         'tbpNewSalary
         '
+        Me.tbpNewSalary.AccessibleDescription = "Employee Salary"
         Me.tbpNewSalary.Controls.Add(Me.SalaryTab)
         Me.tbpNewSalary.Location = New System.Drawing.Point(4, 4)
         Me.tbpNewSalary.Name = "tbpNewSalary"
