4f374dea753ab9eb1eb71fb5ad560934f640ff31
Joshua Santos <jsantos.globagility@gmail.com>
2019-08-27 15:46:07 +0800

    Override position view validation when using user level policy.

--- AccuPay/Views/Initial Forms/HRISForm.vb
+++ AccuPay/Views/Initial Forms/HRISForm.vb
@@ -17,7 +17,7 @@ Public Class HRISForm
 
         Dim formuserprivilege = position_view_table.Select("ViewID = " & view_ID)
 
-        If formuserprivilege.Count > 0 Then
+        If PayrollTools.CheckIfUsingUserLevel() = True OrElse formuserprivilege.Count > 0 Then
 
             For Each drow In formuserprivilege
                 'If drow("ReadOnly").ToString = "Y" Then
