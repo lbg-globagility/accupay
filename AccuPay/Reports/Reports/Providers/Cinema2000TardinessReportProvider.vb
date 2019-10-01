e0fdb8db7954870704e581659c01d077bcde53ed
Joshua Santos <jsantos.globagility@gmail.com>
2019-10-01 11:04:13 +0800

    Move the code to get the organization address to payrolltools.vb class.

--- AccuPay/Reports/Reports/Providers/Cinema2000TardinessReportProvider.vb
+++ AccuPay/Reports/Reports/Providers/Cinema2000TardinessReportProvider.vb
@@ -7,21 +7,8 @@ Public Class Cinema2000TardinessReportProvider
 
     Public Sub Run() Implements IReportProvider.Run
 
-        Dim str_quer_address As String =
-            String.Concat("SELECT CONCAT_WS(', '",
-                          ", IF(LENGTH(TRIM(ad.StreetAddress1)) = 0, NULL, ad.StreetAddress1)",
-                          ", IF(LENGTH(TRIM(ad.StreetAddress2)) = 0, NULL, ad.StreetAddress2)",
-                          ", IF(LENGTH(TRIM(ad.Barangay)) = 0, NULL, ad.Barangay)",
-                          ", IF(LENGTH(TRIM(ad.CityTown)) = 0, NULL, ad.CityTown)",
-                          ", IF(LENGTH(TRIM(ad.Country)) = 0, NULL, ad.Country)",
-                          ", IF(LENGTH(TRIM(ad.State)) = 0, NULL, ad.State)",
-                          ") `Result`",
-                          " FROM organization og",
-                          " LEFT JOIN address ad ON ad.RowID = og.PrimaryAddressID",
-                          " WHERE og.RowID = ", orgztnID, ";")
-
-        Dim params =
-            New Object() {orgztnID}
+        'Dim params =
+        '    New Object() {orgztnID}
 
         'Dim sql As New SQL("CALL RPT_employeeidhistory(?og_rowid);",
         '                   params)
@@ -43,9 +30,7 @@ Public Class Cinema2000TardinessReportProvider
 
             objText = DirectCast(report.ReportDefinition.Sections(2).ReportObjects("txtAddress"), TextObject)
 
-            Dim _address As String =
-                Convert.ToString(New SQL(str_quer_address).GetFoundRow)
-            objText.Text = _address
+            objText.Text = PayrollTools.GetOrganizationAddress()
 
             'report.SetDataSource(dt)
 
--- AccuPay/Reports/Reports/Providers/EmployeeIdentificationNumberReportProvider.vb
+++ AccuPay/Reports/Reports/Providers/EmployeeIdentificationNumberReportProvider.vb
@@ -9,19 +9,6 @@ Public Class EmployeeIdentificationNumberReportProvider
 
     Public Sub Run() Implements IReportProvider.Run
 
-        Dim str_quer_address As String =
-            String.Concat("SELECT CONCAT_WS(', '",
-                          ", IF(LENGTH(TRIM(ad.StreetAddress1)) = 0, NULL, ad.StreetAddress1)",
-                          ", IF(LENGTH(TRIM(ad.StreetAddress2)) = 0, NULL, ad.StreetAddress2)",
-                          ", IF(LENGTH(TRIM(ad.Barangay)) = 0, NULL, ad.Barangay)",
-                          ", IF(LENGTH(TRIM(ad.CityTown)) = 0, NULL, ad.CityTown)",
-                          ", IF(LENGTH(TRIM(ad.Country)) = 0, NULL, ad.Country)",
-                          ", IF(LENGTH(TRIM(ad.State)) = 0, NULL, ad.State)",
-                          ") `Result`",
-                          " FROM organization og",
-                          " LEFT JOIN address ad ON ad.RowID = og.PrimaryAddressID",
-                          " WHERE og.RowID = ", orgztnID, ";")
-
         Dim params =
             New Object() {orgztnID}
 
@@ -45,9 +32,7 @@ Public Class EmployeeIdentificationNumberReportProvider
 
                 objText = DirectCast(report.ReportDefinition.Sections(2).ReportObjects("txtAddress"), TextObject)
 
-                Dim _address As String =
-                    Convert.ToString(New SQL(str_quer_address).GetFoundRow)
-                objText.Text = _address
+                objText.Text = PayrollTools.GetOrganizationAddress()
 
                 report.SetDataSource(dt)
 
@@ -56,7 +41,6 @@ Public Class EmployeeIdentificationNumberReportProvider
                 crvwr.Show()
 
             End If
-
         Catch ex As Exception
             MsgBox(getErrExcptn(ex, Me.Name))
         End Try
