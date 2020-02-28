243c0384f53171a442ee099980b13a3485c84762
beasebastian <bea.globagility@gmail.com>
2020-02-28 11:21:24 +0800

    Fix forgot password.

--- AccuPay/Views/General Forms/ForgotPasswordForm.Designer.vb
+++ AccuPay/Views/General Forms/ForgotPasswordForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class ForgotPasswordForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,69 +20,52 @@ Partial Class ForgotPasswordForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ForgotPasswordForm))
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtUserID = New System.Windows.Forms.TextBox()
-        Me.btnSend = New System.Windows.Forms.Button()
         Me.btnCancel = New System.Windows.Forms.Button()
-        Me.cmbCompany = New System.Windows.Forms.ComboBox()
-        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtUserID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(301, 75)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancel.TabIndex = 7
+        Me.btnCancel.Text = "&Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'btnSend
+        '
+        Me.btnSend.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSend.Location = New System.Drawing.Point(220, 75)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 6
+        Me.btnSend.Text = "&Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtUserID
+        '
+        Me.txtUserID.Font = New System.Drawing.Font("Calibri", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUserID.Location = New System.Drawing.Point(89, 33)
+        Me.txtUserID.Name = "txtUserID"
+        Me.txtUserID.Size = New System.Drawing.Size(287, 23)
+        Me.txtUserID.TabIndex = 5
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(19, 45)
+        Me.Label1.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(22, 33)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(46, 13)
-        Me.Label1.TabIndex = 0
+        Me.Label1.Size = New System.Drawing.Size(61, 19)
+        Me.Label1.TabIndex = 4
         Me.Label1.Text = "User ID:"
-        '
-        'txtUserID
-        '
-        Me.txtUserID.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtUserID.Location = New System.Drawing.Point(113, 42)
-        Me.txtUserID.Name = "txtUserID"
-        Me.txtUserID.Size = New System.Drawing.Size(264, 20)
-        Me.txtUserID.TabIndex = 1
-        '
-        'btnSend
-        '
-        Me.btnSend.Location = New System.Drawing.Point(221, 95)
-        Me.btnSend.Name = "btnSend"
-        Me.btnSend.Size = New System.Drawing.Size(75, 23)
-        Me.btnSend.TabIndex = 2
-        Me.btnSend.Text = "&Send"
-        Me.btnSend.UseVisualStyleBackColor = True
-        '
-        'btnCancel
-        '
-        Me.btnCancel.Location = New System.Drawing.Point(302, 95)
-        Me.btnCancel.Name = "btnCancel"
-        Me.btnCancel.Size = New System.Drawing.Size(75, 23)
-        Me.btnCancel.TabIndex = 3
-        Me.btnCancel.Text = "&Cancel"
-        Me.btnCancel.UseVisualStyleBackColor = True
-        '
-        'cmbCompany
-        '
-        Me.cmbCompany.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.cmbCompany.FormattingEnabled = True
-        Me.cmbCompany.Location = New System.Drawing.Point(113, 15)
-        Me.cmbCompany.Name = "cmbCompany"
-        Me.cmbCompany.Size = New System.Drawing.Size(264, 21)
-        Me.cmbCompany.TabIndex = 0
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(19, 18)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(88, 13)
-        Me.Label3.TabIndex = 7
-        Me.Label3.Text = "Company Name::"
         '
         'ForgotPasswordForm
         '
@@ -90,8 +73,6 @@ Partial Class ForgotPasswordForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer), CType(CType(242, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(399, 130)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.cmbCompany)
         Me.Controls.Add(Me.btnCancel)
         Me.Controls.Add(Me.btnSend)
         Me.Controls.Add(Me.txtUserID)
@@ -108,10 +89,9 @@ Partial Class ForgotPasswordForm
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents txtUserID As System.Windows.Forms.TextBox
-    Friend WithEvents btnSend As System.Windows.Forms.Button
-    Friend WithEvents btnCancel As System.Windows.Forms.Button
-    Friend WithEvents cmbCompany As System.Windows.Forms.ComboBox
-    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents btnSend As Button
+    Friend WithEvents txtUserID As TextBox
+    Friend WithEvents Label1 As Label
 End Class
--- AccuPay/Views/General Forms/ForgotPasswordForm.vb
+++ AccuPay/Views/General Forms/ForgotPasswordForm.vb
@@ -1,65 +1,65 @@
-﻿Imports System.Net.Mail
+﻿Imports System.Net
+Imports System.Net.Mail
 
 Public Class ForgotPasswordForm
-    Sub fillPosition()
-        Dim strQuery As String = "select Name from Organization"
-        cmbCompany.Items.Clear()
-        cmbCompany.Items.Add("-Select One-")
-        cmbCompany.Items.AddRange(CType(SQL_ArrayList(strQuery).ToArray(GetType(String)), String()))
-        cmbCompany.SelectedIndex = 0
-    End Sub
+
     Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
-        'If txtEmailAdd.Text = "" Or txtUserID.Text = "" Or cmbCompany.Text = "--Select One" Then
-        '    MsgBox("Please complete all fields.", MsgBoxStyle.Exclamation, "System message")
-        'Else
 
-        If cmbCompany.Text = "-Select One-" Then
-            MsgBox("Please select your company.", MsgBoxStyle.Exclamation, "System message")
-        ElseIf txtUserID.Text = "" Then
+
+        If txtUserID.Text = "" Then
             MsgBox("Please enter your User ID", MsgBoxStyle.Exclamation, "System message.")
-        Else
-            Dim orgID As String = getStringItem("Select RowID from organization where name = '" & cmbCompany.Text & "'")
-            Dim getID As Integer = Val(orgID)
-            Dim emailadd As String = getStringItem("Select EmailAddress from user where UserID = '" & EncrypedData(txtUserID.Text) & "' And OrganizationID = " & getID & "")
-            Dim geteamiladd As String = emailadd
-
-            If emailadd = "" Then
-                MsgBox("You don't have an email address, the system failed to send your password.", MsgBoxStyle.Exclamation, "No email address detected")
-                Exit Sub
-            End If
-
-            Try
-                Dim org As String = getStringItem("Select RowID from Organization where name = '" & cmbCompany.Text & "'")
-                Dim getOrgID As Integer = Val(org)
-                Dim pw As String = getStringItem("Select Password from user where userid = '" & EncrypedData(txtUserID.Text) & "' And OrganizationID = " & getOrgID & "")
-                Dim getpw As String = DecrypedData(pw)
-                Dim stmp_server As New SmtpClient
-                Dim email As New MailMessage
-                stmp_server.UseDefaultCredentials = False
-
-                stmp_server.Credentials = New Net.NetworkCredential("testemailforglobagility@gmail.com", "PIN4545global")
-                stmp_server.Port = 587
-                stmp_server.EnableSsl = True
-                stmp_server.Host = "smtp.gmail.com"
-
-                email = New MailMessage()
-                email.From = New MailAddress("testemailforglobagility@gmail.com")
-                email.To.Add(geteamiladd)
-                email.Subject = "Forgot password."
-                email.IsBodyHtml = False
-                email.Body = "Your password is " & getpw & ""
-                stmp_server.Send(email)
-
-                MsgBox("Your Password will be sent to the email address associated to your user name", MsgBoxStyle.Information, "Sent Successfully.")
-
-                Me.Close()
-            Catch ex As Exception
-                '"No Connection."
-                MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Sending password failed") 'No Internet Connection.
-            End Try
 
         End If
 
+        Dim emailadd As String = getStringItem("Select EmailAddress from user where UserID = '" & EncrypedData(txtUserID.Text) & "'")
+        Dim getemailadd As String = emailadd
+
+        If emailadd = "" Then
+            MsgBox("You don't have an email address, the system failed to send your password.", MsgBoxStyle.Exclamation, "No email address detected")
+            Exit Sub
+        End If
+
+        Try
+            Dim pw As String = getStringItem("Select Password from user where userid = '" & EncrypedData(txtUserID.Text) & "'")
+            Dim getpw As String = DecrypedData(pw)
+
+            SendEmail(emailadd, getpw)
+
+            MsgBox("Your Password will be sent to the email address associated to your user name", MsgBoxStyle.Information, "Sent Successfully.")
+
+            Me.Close()
+        Catch ex As Exception
+            '"No Connection."
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation, "Sending password failed") 'No Internet Connection.
+        End Try
+
+
+
+    End Sub
+
+    Private Shared Sub SendEmail(sendTo As String, getpw As String)
+        Dim mail As MailMessage = New MailMessage()
+        Dim SmtpServer As SmtpClient = New SmtpClient()
+        Dim smtpsender As String = "testemailforglobagility@gmail.com"
+        Dim subject As String = "Forgot Password"
+        Dim body As String = "Your password is " & getpw & ""
+        Dim smtpDisplayName As String = "Accupay"
+        Dim smtpHost As String = "smtp.gmail.com"
+        Dim smtpPort As Integer = 587
+        Dim smtpPassword As String = "PIN4545global"
+        mail.From = New MailAddress(smtpsender, smtpDisplayName)
+        mail.[To].Add(sendTo)
+        mail.Subject = subject
+        mail.Body = body
+
+
+        SmtpServer.Host = smtpHost
+        SmtpServer.Port = smtpPort
+        SmtpServer.UseDefaultCredentials = False
+        SmtpServer.Credentials = New NetworkCredential(smtpsender, smtpPassword)
+        SmtpServer.EnableSsl = True
+        SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network
+        SmtpServer.Send(mail)
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
@@ -78,12 +78,9 @@ Public Class ForgotPasswordForm
 
     End Sub
 
-    Private Sub ForgotPasswordForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        fillPosition()
-
-    End Sub
 
     Private Sub txtUserID_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtUserID.KeyPress
+
         Dim e_asc As String = Asc(e.KeyChar)
 
         If e_asc = 13 Then
@@ -91,7 +88,6 @@ Public Class ForgotPasswordForm
         End If
 
     End Sub
-
     Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
 
         If keyData = Keys.Escape Then
