247e481d5505b519bf252bde2bf248505009ae3a
aaron_delrosario <aaron.delrosario09@gmail.com>
2020-04-06 18:10:46 +0800

    feat(#5): init leave accrual

---
+++ AccuPay/Domain/Payroll/LeaveAccrualService.vb
@@ -0,0 +1,47 @@
+﻿Option Strict On
+
+Imports AccuPay.Entity
+Imports AccuPay.Utilities
+Imports Microsoft.EntityFrameworkCore
+
+Public Class LeaveAccrualService
+
+    Private _daysInPayrollYear As Integer
+
+    Public Sub New()
+        _daysInPayrollYear = 366
+    End Sub
+
+    Public Async Sub ComputeAccrual(employee As Employee, payperiod As PayPeriod)
+        Using context = New PayrollContext()
+            Dim firstPayperiod = Await context.PayPeriods.
+                Where(Function(p) p.Year = payperiod.Year AndAlso p.Month = 1).
+                FirstOrDefaultAsync()
+
+            Dim startDateOfYear = firstPayperiod.PayFromDate
+
+            Dim timeSinceLastCutoff = payperiod.PayFromDate.AddDays(-1) - startDateOfYear
+            Dim leaveSinceLastCutoff = ComputeLeaveAmount(timeSinceLastCutoff, employee)
+
+            Dim timeSinceCurrentCutoff = payperiod.PayToDate - startDateOfYear
+            Dim leaveSinceCurrentCutoff = ComputeLeaveAmount(timeSinceCurrentCutoff, employee)
+
+            ' Get the difference
+            Dim leaveForCutoff = leaveSinceLastCutoff - leaveSinceCurrentCutoff
+        End Using
+    End Sub
+
+    Private Function ComputeLeaveAmount(time As TimeSpan, employee As Employee) As Decimal
+        Dim leaves = (time.Days / 366D) * employee.SickLeaveAllowance
+
+        Return AccuMath.CommercialRound(leaves)
+    End Function
+
+    Private Function GetDaysInYear(year As Integer) As Integer
+        Dim firstDay = New DateTime(year, 1, 1)
+        Dim lastDay = New DateTime(year, 12, 31)
+
+        Return (lastDay - firstDay).Days
+    End Function
+
+End Class
---
+++ TestPayrollSystem/LeaveAccrualServiceTest.vb
@@ -0,0 +1,33 @@
+﻿Option Strict On
+
+Imports AccuPay
+Imports AccuPay.Entity
+
+<TestFixture>
+Public Class LeaveAccrualServiceTest
+
+    <Test>
+    Public Sub ShouldGiveLeaves()
+        ' Arrange
+        Dim employee = New Employee() With {
+            .VacationLeaveAllowance = 40,
+            .SickLeaveAllowance = 40
+        }
+
+        Dim payperiod = New PayPeriod() With {
+            .PayFromDate = New Date(2020, 1, 1),
+            .PayToDate = New Date(2020, 1, 15),
+            .Month = 3,
+            .Year = 2020
+        }
+
+        Dim service = New LeaveAccrualService()
+
+        ' Act
+        service.ComputeAccrual(employee, payperiod)
+
+        ' Assert
+        ' No assertions yet
+    End Sub
+
+End Class
