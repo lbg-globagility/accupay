9e96b65ef2fd5252125513bc6f424319a3ba3d98
Lambert Galit <lbg.globagility@gmail.com>
2020-04-08 18:20:00 +0800

    bpi insurance payment report

--- AccuPay/Reports/Reports/Providers/LA Global/BpiInsurancePaymentReportProvider.vb
+++ AccuPay/Reports/Reports/Providers/LA Global/BpiInsurancePaymentReportProvider.vb
@@ -22,9 +22,10 @@ Public Class BpiInsurancePaymentReportProvider
             Dim parameterSetter = New CrystalReportParameterValueSetter(reportDocument)
             With parameterSetter
                 .SetParameter("selectedDate", _selectedDate)
-                .SetParameter("noRecordFound", True)
 
             End With
+
+            SetDataSource(reportDocument)
 
             Dim form = New LaGlobalEmployeeReportForm
             form.reportViewer.ReportSource = reportDocument
@@ -37,7 +38,7 @@ Public Class BpiInsurancePaymentReportProvider
         Return succeed
     End Function
 
-    Private Async Sub SetDataSource(reportClass As ReportClass, parameterSetter As CrystalReportParameterValueSetter)
+    Private Async Sub SetDataSource(reportClass As ReportClass) ', parameterSetter As CrystalReportParameterValueSetter
 
         Using context = New PayrollContext
             Dim periods = Await context.PayPeriods.Where(Function(p) p.OrganizationID.Value = z_OrganizationID).
@@ -48,9 +49,58 @@ Public Class BpiInsurancePaymentReportProvider
 
             Dim periodIDs = periods.Select(Function(p) p.RowID.Value).ToArray()
 
+            Dim productNames = {"Bpi Insurance", "Non Taxable Adjustment"}
 
+            Dim bpiInsuranceProductIDs = Await context.Products.
+                Where(Function(p) p.OrganizationID.Value = z_OrganizationID).
+                Where(Function(p) p.Category = "Adjustment Type").
+                Where(Function(p) productNames.Contains(p.Name)).
+                Select(Function(p) p.RowID.Value).
+                ToListAsync()
+
+            Dim adjustmens = Await context.Adjustments.
+                Include(Function(a) a.Paystub.Employee).
+                Where(Function(a) periodIDs.Contains(a.Paystub.PayPeriodID.Value)).
+                Where(Function(a) bpiInsuranceProductIDs.Contains(a.ProductID.Value)).
+                ToListAsync()
+
+            Dim source = adjustmens.
+                GroupBy(Function(a) a.Paystub.EmployeeID).
+                Select(Function(a) ConvertToDataSource(a)).
+                OrderBy(Function(a) a.Column2).
+                ToList()
+
+            'parameterSetter.SetParameter("noRecordFound", Not source.Any())
+
+            reportClass.SetDataSource(source)
         End Using
-        'reportClass.SetDataSource()
     End Sub
 
+    Private Function ConvertToDataSource(a As IGrouping(Of Integer?, Adjustment)) As BpiInsuranceDateSource
+        Dim e = a.FirstOrDefault.Paystub.Employee
+        Dim middleName = If(Not String.IsNullOrWhiteSpace(e.MiddleName), $"{Left(e.MiddleName, 1)}.", String.Empty)
+        Dim nameParts = {e.LastName, e.FirstName, middleName}
+        Dim fullName = String.Join(", ", nameParts.Where(Function(s) Not String.IsNullOrWhiteSpace(s)).ToArray())
+
+        Dim result As New BpiInsuranceDateSource With {
+            .Column1 = e.EmployeeNo,
+            .Column2 = fullName,
+            .Column3 = a.Sum(Function(adj) adj.Amount),
+            .Column4 = _selectedDate.ToShortDateString()}
+
+        Return result
+    End Function
+
+    Private Class BpiInsuranceDateSource
+        'Employee ID
+        Public Property Column1 As String
+
+        'Employee Fullname
+        Public Property Column2 As String
+
+        'Payment/Amount
+        Public Property Column3 As String
+        'Selected Month - in a value of date
+        Public Property Column4 As String
+    End Class
 End Class
