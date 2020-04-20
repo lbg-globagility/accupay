340244b0bf40c83889d7a72d7e579ff6882e5558
Lambert Galit <lbg.globagility@gmail.com>
2020-04-20 15:11:24 +0800

    add PositionRepository class

---
+++ Accupay.Data/Repositories/PositionRepository.cs
@@ -0,0 +1,23 @@
+﻿using AccuPay.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccuPay.Data.Repositories
+{
+    public class PositionRepository
+    {
+        public async Task<Position> GetByName(int organizationID, string positionName)
+        {
+            using (PayrollContext context = new PayrollContext())
+            {
+                return await context.Positions.
+                    Where(p => p.OrganizationID == organizationID).
+                    Where(p => p.Name == positionName).
+                    FirstOrDefaultAsync();
+            }
+        }
+    }
+}
