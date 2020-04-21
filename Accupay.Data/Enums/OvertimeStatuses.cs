fadff18af66c2be6940ddeb68c0319b9808817c9
Lambert Galit <lbg.globagility@gmail.com>
2020-04-21 14:11:28 +0800

    add enums of overtime statuses

---
+++ Accupay.Data/Enums/OvertimeStatuses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccuPay.Data.Enums
+{
+    public enum OvertimeStatuses
+    {
+        All = 0,
+        Approved = 1,
+        Pending = 2
+    }
+}
