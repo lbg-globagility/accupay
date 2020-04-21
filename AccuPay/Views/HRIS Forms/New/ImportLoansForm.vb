930ee594eb7ee925590dfac862cb680cdcb0ac72
Joshua Santos <joshuanoelsantos@gmail.com>
2020-04-21 15:32:55 +0800

    Validate loan start date on import should not be earlier than January 1, 1753.

--- AccuPay/Views/HRIS Forms/New/ImportLoansForm.vb
+++ AccuPay/Views/HRIS Forms/New/ImportLoansForm.vb
@@ -133,7 +133,7 @@ Public Class ImportLoansForm
                 .Comments = record.Comments,
                 .TotalLoanAmount = record.TotalLoanAmount,
                 .TotalBalanceLeft = record.TotalBalanceLeft,
-                .DedEffectiveDateFrom = record.DedEffectiveDateFrom,
+                .DedEffectiveDateFrom = record.StartDate,
                 .DeductionAmount = record.DeductionAmount,
                 .DeductionPercentage = 0,
                 .LoanName = record.LoanName,
@@ -161,9 +161,16 @@ Public Class ImportLoansForm
 
     Private Function CheckIfRecordIsValid(record As LoanRowRecord, rejectedRecords As List(Of LoanRowRecord)) As Boolean
 
-        If record.DedEffectiveDateFrom Is Nothing Then
+        If record.StartDate Is Nothing Then
 
             record.ErrorMessage = "Start date cannot be empty."
+            rejectedRecords.Add(record)
+            Return False
+        End If
+
+        If record.StartDate < PayrollTools.MinimumMicrosoftDate Then
+
+            record.ErrorMessage = "dates cannot be earlier than January 1, 1753."
             rejectedRecords.Add(record)
             Return False
         End If
