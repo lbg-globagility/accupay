0863899b2868807a9eee89aafa6615fcd81f6128
Lambert Galit <lbg.globagility@gmail.com>
2020-04-22 16:34:05 +0800

    monthly end of contract report, descending order of regularization date, etc

--- AccuPay/Reports/Reports/Providers/LA Global/MonthlyEndofContractReportProvider.vb
+++ AccuPay/Reports/Reports/Providers/LA Global/MonthlyEndofContractReportProvider.vb
@@ -1,5 +1,5 @@
 ﻿Imports AccuPay.Entity
-Imports Microsoft.EntityFrameworkCore
+Imports AccuPay.Repository
 
 Public Class MonthlyEndofContractReportProvider
     Implements ILaGlobalEmployeeReport
@@ -11,6 +11,8 @@ Public Class MonthlyEndofContractReportProvider
     Private _startDate As Date
 
     Private _endDate As Date
+
+    Private employeeRepo As New EmployeeRepository()
 
     Public Property Employee As Employee Implements ILaGlobalEmployeeReport.Employee
 
@@ -36,34 +38,33 @@ Public Class MonthlyEndofContractReportProvider
     End Function
 
     Private Async Sub SetDataSource()
-        Using context = New PayrollContext
-            Dim employees = Await context.Employees.
-                Where(Function(e) e.OrganizationID.Value = z_OrganizationID).
-                Where(Function(e) e.IsActive).
-                Where(Function(e) e.DateRegularized.Value.Date >= _startDate AndAlso e.DateRegularized.Value.Date <= _endDate).
-                ToListAsync()
+        Dim fetchAll = Await employeeRepo.GetAllAsync()
+        Dim employees = fetchAll.
+            Where(Function(e) e.OrganizationID.Value = z_OrganizationID).
+            Where(Function(e) e.IsActive).
+            Where(Function(e) e.DateRegularized.HasValue).
+            Where(Function(e) e.DateRegularized.Value.Date >= _startDate AndAlso e.DateRegularized.Value.Date <= _endDate).
+            ToList()
 
-            recordFound = employees.Any()
-            If Not recordFound Then
-                MessageBox.Show($"No record found.", "Active Employee Checklist Report", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        recordFound = employees.Any()
+        If Not recordFound Then
+            MessageBox.Show($"No record found.", "Active Employee Checklist Report", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
-                Return
-            End If
+            Return
+        End If
 
-            Dim ascendingOrder = employees.OrderBy(Function(e) e.DateRegularized.Value.Date).ToList()
+        Dim descendingOrder = employees.OrderByDescending(Function(e) e.DateRegularized.Value.Date).ToList()
 
-            Dim source = ascendingOrder.
-                Select(Function(e) New ActiveEmployeeChecklistDateSource With {
-                .Column1 = e.EmployeeNo,
-                .Column2 = $"{e.LastName}, {e.FirstName}{If(Not String.IsNullOrWhiteSpace(e.MiddleName), $", {Left(e.MiddleName, 1)}.", String.Empty)}",
-                .Column3 = e.Branch?.Name,
-                .Column4 = e.StartDate.ToShortDateString(),
-                .Column5 = If(e.DateRegularized.HasValue, e.DateRegularized.Value.Date.ToShortDateString(), String.Empty)}).
-                ToList()
+        Dim source = descendingOrder.
+            Select(Function(e) New ActiveEmployeeChecklistDateSource With {
+            .Column1 = e.EmployeeNo,
+            .Column2 = $"{e.LastName}, {e.FirstName}{If(Not String.IsNullOrWhiteSpace(e.MiddleName), $", {Left(e.MiddleName, 1)}.", String.Empty)}",
+            .Column3 = e.Branch?.Name,
+            .Column4 = e.StartDate.ToShortDateString(),
+            .Column5 = If(e.DateRegularized.HasValue, e.DateRegularized.Value.Date.ToShortDateString(), String.Empty)}).
+            ToList()
 
-            _reportDocument.SetDataSource(source)
-
-        End Using
+        _reportDocument.SetDataSource(source)
 
         Dim parameterSetter = New CrystalReportParameterValueSetter(_reportDocument)
         Dim parameters = New Dictionary(Of String, Object) From {
