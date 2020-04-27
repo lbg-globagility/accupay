dc800af79a1c076f58792aa7c84c77b88d47c9af
Lambert Galit <lbg.globagility@gmail.com>
2020-04-27 12:27:23 +0800

    declare the given user statuses

---
+++ Accupay.Data/Helpers/UserConstants.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AccuPay.Data.Helpers
+{
+    public class UserConstants
+    {
+        public enum UserStatus
+        {
+            Active = 0,
+            Inactive = 1
+        }
+
+        internal static string ACTIVE_STATUS
+        {
+            get
+            {
+                return UserStatus.Active.ToString();
+            }
+        }
+
+        internal static string INACTIVE_STATUS
+        {
+            get
+            {
+                return UserStatus.Inactive.ToString();
+            }
+        }
+    }
+}
