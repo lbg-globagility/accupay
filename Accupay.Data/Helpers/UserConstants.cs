fbf40c02ffe5cc0e5e39d31d3a6b53073511d25b
Lambert Galit <lbg.globagility@gmail.com>
2020-04-28 17:58:16 +0800

    function for converting UserStatus to string

--- Accupay.Data/Helpers/UserConstants.cs
+++ Accupay.Data/Helpers/UserConstants.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace AccuPay.Data.Helpers
 {
     public class UserConstants
     {
+        private static Dictionary<UserStatus, string> indentifyUserStatus =
+            new Dictionary<UserStatus, string>() {
+                { UserStatus.Active, UserStatus.Active.ToString() },
+                { UserStatus.Inactive, UserStatus.Inactive.ToString() }
+            };
+
         public enum UserStatus
         {
             Active = 0,
             Inactive = 1
+        }
+
+        internal static string UserStatusToString(UserStatus userStatus)
+        {
+            return indentifyUserStatus[userStatus];
         }
 
         internal static string ACTIVE_STATUS
