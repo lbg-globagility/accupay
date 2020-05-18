0c4fa3bd422e8e1575216d5474ca12003d9b34dd
Lambert Galit <lbg.globagility@gmail.com>
2020-05-18 16:27:11 +0800

    feat(employee): add clien-side service for requesting employee paginated list
    Task#7

---
+++ AccuPay.Client/src/app/employees/services/employee.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { PageOptions } from 'src/app/core/shared/page-options';
+import { Observable } from 'rxjs';
+import { PaginatedList } from 'src/app/core/shared/paginated-list';
+import { Employee } from '../shared/employee';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  apiRoute = 'api/employees';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getList(
+    options: PageOptions,
+    term = ''
+  ): Observable<PaginatedList<Employee>> {
+    const params = options ? options.toObject() : null;
+    params.term = term;
+
+    return this.httpClient.get<PaginatedList<Employee>>(this.apiRoute, {
+      params,
+    });
+  }
+}
