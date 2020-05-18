9f936d918db7224bb52b842b022746218d0127c2
Lambert Galit <lbg.globagility@gmail.com>
2020-05-18 16:25:05 +0800

    feat(employee): api method for getting all employee list base on organizationId
    Task#7

--- AccuPay.Web/Controllers/EmployeesController.cs
+++ AccuPay.Web/Controllers/EmployeesController.cs
@@ -1,8 +1,11 @@
-using AccuPay.Data.Repositories;
+using Accupay.Web.Core.Auth;
+using AccuPay.Data.Helpers;
+using AccuPay.Web.Core.Extensions;
 using AccuPay.Web.Employees.Models;
 using AccuPay.Web.Employees.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AccuPay.Web.Controllers
@@ -12,10 +15,12 @@ namespace AccuPay.Web.Controllers
     public class EmployeesController : ControllerBase
     {
         private readonly EmployeeService _employeeService;
+        private readonly CurrentUser _currentUser;
 
-        public EmployeesController(EmployeeService employeeService)
+        public EmployeesController(EmployeeService employeeService, CurrentUser currentUser)
         {
             _employeeService = employeeService;
+            _currentUser = currentUser;
         }
 
         [HttpPost]
@@ -52,6 +57,31 @@ namespace AccuPay.Web.Controllers
             var dto = await _employeeService.GeyByIdAsync(id);
 
             return dto;
+        }
+
+        [HttpGet]
+        public async Task<PaginatedList<EmployeeDto>> List([FromQuery] PageOptions options, string term = "")
+        {
+            int variableOrganizationId = 5;//_currentUser.OrganizationId
+            var query = await _employeeService.GetAllAsync(variableOrganizationId);
+
+            if (!string.IsNullOrWhiteSpace(term))
+            {
+                query = query.Where(s => s.FullNameWithMiddleInitialLastNameFirst.Contains(term, StringComparison.OrdinalIgnoreCase));
+            }
+
+            query = options.Sort switch
+            {
+                "name" => query.OrderBy(t => t.FullNameWithMiddleInitialLastNameFirst, options.Direction),
+                _ => query.OrderBy(t => t.FullNameWithMiddleInitialLastNameFirst)
+            };
+
+            var roles = query.Page(options).ToList();
+            var count = query.Count();
+
+            var dtos = roles.Select(s => EmployeeDto.Convert(s)).ToList();
+
+            return new PaginatedList<EmployeeDto>(dtos, count, options.PageIndex, options.PageSize);
         }
     }
 }
