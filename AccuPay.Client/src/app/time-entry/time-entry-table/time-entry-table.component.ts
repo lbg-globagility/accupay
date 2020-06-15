3ea4843efed12ab5117067a32f8a1927a52f1be1
Joshua Santos <jsantos.globagility@gmail.com>
2020-06-15 17:22:47 +0800

    feat(#55): hide the columns that does not have value in daily breakdown of employee hours

--- AccuPay.Client/src/app/time-entry/time-entry-table/time-entry-table.component.ts
+++ AccuPay.Client/src/app/time-entry/time-entry-table/time-entry-table.component.ts
@@ -1,12 +1,13 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { TimeEntry } from 'src/app/time-entry/shared/time-entry';
+import { filter } from 'lodash';
+
+enum FilterType {
+  Number,
+  Object,
+  Array,
+}
 
 @Component({
   selector: 'app-time-entry-table',
@@ -17,7 +18,7 @@ export class TimeEntryTableComponent implements OnChanges {
   @Input()
   timeEntries: TimeEntry[] = [];
 
-  readonly displayedColumns = [
+  readonly allColumns = [
     'date',
     'shift',
     'timeLog',
@@ -53,9 +54,66 @@ export class TimeEntryTableComponent implements OnChanges {
     'absentDeduction',
   ];
 
+  displayedColumns: string[];
+
   dataSource: MatTableDataSource<TimeEntry>;
 
-  ngOnChanges(changes: SimpleChanges): void {
+  constructor() {
+    this.displayedColumns = this.allColumns;
+  }
+  ngOnChanges(): void {
+    if (this.timeEntries == null) return;
+
+    this.displayedColumns = this.allColumns.slice();
+    this.unHideColumn('officialBusiness', FilterType.Object);
+    this.unHideColumn('leave', FilterType.Object);
+    this.unHideColumn('leaveHours', FilterType.Number);
+    this.unHideColumn('leavePay', FilterType.Number);
+    this.unHideColumn('overtimes', FilterType.Array);
+    this.unHideColumn('overtimeHours', FilterType.Number);
+    this.unHideColumn('overtimePay', FilterType.Number);
+    this.unHideColumn('nightDiffHours', FilterType.Number);
+    this.unHideColumn('nightDiffPay', FilterType.Number);
+    this.unHideColumn('nightDiffOTHours', FilterType.Number);
+    this.unHideColumn('nightDiffOTPay', FilterType.Number);
+    this.unHideColumn('restDayHours', FilterType.Number);
+    this.unHideColumn('restDayAmount', FilterType.Number);
+    this.unHideColumn('restDayOTHours', FilterType.Number);
+    this.unHideColumn('restDayOTPay', FilterType.Number);
+    this.unHideColumn('specialHolidayHours', FilterType.Number);
+    this.unHideColumn('specialHolidayPay', FilterType.Number);
+    this.unHideColumn('specialHolidayOTHours', FilterType.Number);
+    this.unHideColumn('specialHolidayOTPay', FilterType.Number);
+    this.unHideColumn('regularHolidayHours', FilterType.Number);
+    this.unHideColumn('regularHolidayPay', FilterType.Number);
+    this.unHideColumn('regularHolidayOTHours', FilterType.Number);
+    this.unHideColumn('regularHolidayOTPay', FilterType.Number);
     this.dataSource = new MatTableDataSource(this.timeEntries);
+  }
+
+  private unHideColumn(key: string, type: FilterType) {
+    let condition: Function;
+
+    switch (type) {
+      case FilterType.Number:
+        condition = (t) => t[key] != null && t[key] != 0;
+        break;
+
+      case FilterType.Array:
+        condition = (t) => t[key] != null && t[key].length > 0;
+        break;
+
+      case FilterType.Object:
+        condition = (t) => t[key] != null;
+        break;
+    }
+
+    // var fil = filter(this.timeEntries, (t) => {
+    //   return t.leave != null;
+    // });
+
+    if (filter(this.timeEntries, condition).length == 0) {
+      this.displayedColumns.splice(this.displayedColumns.indexOf(key), 1);
+    }
   }
 }
