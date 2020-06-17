b3a00a35c5ff035d8db8db0d9d74d15913c138f6
aaron_delrosario <aaron.delrosario09@gmail.com>
2020-06-17 13:59:36 +0800

    feat: improve look of time entry table

--- AccuPay.Client/src/app/core/pipes/if-zero.pipe.ts
+++ AccuPay.Client/src/app/core/pipes/if-zero.pipe.ts
@@ -5,9 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IfZeroPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
-    let input = Number(value);
+    if (!isFinite(value)) {
+      return value;
+    }
 
-    if (isNaN(input) || input == 0) {
+    const input = Number(value);
+
+    if (isNaN(input) || input === 0) {
       const alternative = args.length > 0 ? args[0] : '-';
       return alternative;
     } else {
