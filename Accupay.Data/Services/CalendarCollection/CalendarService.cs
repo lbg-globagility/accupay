fa8802c91de4682c13f5a657afb65222a87074fd
aaron_delrosario <aaron.delrosario09@gmail.com>
2020-07-14 13:58:10 +0800

    feat(#93): add default payrates when payrate is missing

--- Accupay.Data/Services/CalendarCollection/CalendarService.cs
+++ Accupay.Data/Services/CalendarCollection/CalendarService.cs
@@ -1,9 +1,7 @@
-﻿using AccuPay.Data.Entities;
-using AccuPay.Data.Enums;
+﻿using AccuPay.Data.Enums;
 using AccuPay.Data.Repositories;
 using AccuPay.Data.ValueObjects;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AccuPay.Data.Services
@@ -25,24 +23,61 @@ namespace AccuPay.Data.Services
             int organizationId)
         {
             var payrates = _context.PayRates.
-                                Where(p => p.OrganizationID == organizationId).
-                                Where(p => timePeriod.Start <= p.Date).
-                                Where(p => p.Date <= timePeriod.End).
-                                ToList();
+                Where(p => p.OrganizationID == organizationId).
+                Where(p => timePeriod.Start <= p.Date).
+                Where(p => p.Date <= timePeriod.End).
+                ToList();
+
             if (calculationBasis == PayRateCalculationBasis.Branch)
             {
+                var dayType = _context.DayTypes.FirstOrDefault(t => t.Name == "Regular Day");
+
+                var defaultRates = new DefaultRates()
+                {
+                    RegularRate = dayType.RegularRate,
+                    OvertimeRate = dayType.OvertimeRate,
+                    NightDiffRate = dayType.NightDiffRate,
+                    NightDiffOTRate = dayType.NightDiffOTRate,
+                    RestDayRate = dayType.RestDayRate,
+                    RestDayOTRate = dayType.RestDayOTRate,
+                    RestDayNDRate = dayType.RestDayNDRate,
+                    RestDayNDOTRate = dayType.RestDayNDOTRate,
+                };
+
                 var branches = _branchRepository.GetAll();
 
                 var calendarDays = _context.CalendarDays.
-                                            Include(t => t.DayType).
-                                            Where(t => timePeriod.Start <= t.Date).
-                                            Where(t => t.Date <= timePeriod.End).
-                                            ToList();
+                    Include(t => t.DayType).
+                    Where(t => timePeriod.Start <= t.Date).
+                    Where(t => t.Date <= timePeriod.End).
+                    ToList();
 
-                return new CalendarCollection(payrates, (ICollection<Branch>)branches, calendarDays, organizationId);
+                return new CalendarCollection(payrates, branches, calendarDays, organizationId, defaultRates);
             }
             else
-                return new CalendarCollection(payrates, organizationId);
+            {
+                var regularDayListofValue = _context.ListOfValues
+                    .FirstOrDefault(t => t.Type == "Pay rate" && t.LIC == "Regular Day");
+
+                var values = regularDayListofValue.DisplayValue
+                    .Split(',')
+                    .Select(t => decimal.Parse(t) / 100)
+                    .ToArray();
+
+                var defaultRates = new DefaultRates()
+                {
+                    RegularRate = values[0],
+                    OvertimeRate = values[1],
+                    NightDiffRate = values[2],
+                    NightDiffOTRate = values[3],
+                    RestDayRate = values[4],
+                    RestDayOTRate = values[5],
+                    RestDayNDRate = values[6],
+                    RestDayNDOTRate = values[7],
+                };
+
+                return new CalendarCollection(payrates, organizationId, defaultRates);
+            }
         }
     }
 }
---
+++ Accupay.Data/Services/CalendarCollection/DefaultPayrate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AccuPay.Data.Services
+{
+    public class DefaultPayrate : IPayrate
+    {
+        private readonly DefaultRates _defaultRates;
+
+        public DefaultPayrate(
+            DateTime date,
+            DefaultRates defaultRates)
+        {
+            Date = date;
+            _defaultRates = defaultRates;
+        }
+
+        public DateTime Date { get; private set; }
+
+        public decimal RegularRate => _defaultRates.RegularRate;
+
+        public decimal OvertimeRate => _defaultRates.OvertimeRate;
+
+        public decimal NightDiffRate => _defaultRates.NightDiffRate;
+
+        public decimal NightDiffOTRate => _defaultRates.NightDiffOTRate;
+
+        public decimal RestDayRate => _defaultRates.RestDayRate;
+
+        public decimal RestDayOTRate => _defaultRates.RestDayOTRate;
+
+        public decimal RestDayNDRate => _defaultRates.RestDayNDRate;
+
+        public decimal RestDayNDOTRate => _defaultRates.RestDayNDOTRate;
+
+        public bool IsRegularDay => true;
+
+        public bool IsRegularHoliday => false;
+
+        public bool IsSpecialNonWorkingHoliday => false;
+
+        public bool IsHoliday => false;
+
+        public bool IsDoubleHoliday => false;
+    }
+}
