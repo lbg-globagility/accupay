941858fdc51a80a05af495ef3c745695d0568dad
Aaron <aaron.delrosario09@gmail.com>
2020-07-23 15:07:35 +0800

    feat(#111): add permission types

---
+++ AccuPay.Client/src/app/core/auth/permission-types.ts
@@ -0,0 +1,96 @@
+export enum PermissionTypes {
+  AccountRead = 'Account:read',
+  AccountCreate = 'Account:create',
+  AccountUpdate = 'Account:update',
+  AccountDelete = 'Account:delete',
+
+  AllowanceRead = 'Allowance:read',
+  AllowanceCreate = 'Allowance:create',
+  AllowanceUpdate = 'Allowance:update',
+  AllowanceDelete = 'Allowance:delete',
+
+  BranchRead = 'Branch:read',
+  BranchCreate = 'Branch:create',
+  BranchUpdate = 'Branch:update',
+  BranchDelete = 'Branch:delete',
+
+  CalendarRead = 'Calendar:read',
+  CalendarCreate = 'Calendar:create',
+  CalendarUpdate = 'Calendar:update',
+  CalendarDelete = 'Calendar:delete',
+
+  DivisionRead = 'Division:read',
+  DivisionCreate = 'Division:create',
+  DivisionUpdate = 'Division:update',
+  DivisionDelete = 'Division:delete',
+
+  EmployeeRead = 'Employee:read',
+  EmployeeCreate = 'Employee:create',
+  EmployeeUpdate = 'Employee:update',
+  EmployeeDelete = 'Employee:delete',
+
+  LeaveRead = 'Leave:read',
+  LeaveCreate = 'Leave:create',
+  LeaveUpdate = 'Leave:update',
+  LeaveDelete = 'Leave:delete',
+
+  LoanRead = 'Loan:read',
+  LoanCreate = 'Loan:create',
+  LoanUpdate = 'Loan:update',
+  LoanDelete = 'Loan:delete',
+
+  OfficialBusinessRead = 'OfficialBusiness:read',
+  OfficialBusinessCreate = 'OfficialBusiness:create',
+  OfficialBusinessUpdate = 'OfficialBusiness:update',
+  OfficialBusinessDelete = 'OfficialBusiness:delete',
+
+  OrganizationRead = 'Organization:read',
+  OrganizationCreate = 'Organization:create',
+  OrganizationUpdate = 'Organization:update',
+  OrganizationDelete = 'Organization:delete',
+
+  OvertimeRead = 'Overtime:read',
+  OvertimeCreate = 'Overtime:create',
+  OvertimeUpdate = 'Overtime:update',
+  OvertimeDelete = 'Overtime:delete',
+
+  PayPeriodRead = 'PayPeriod:read',
+  PayPeriodCreate = 'PayPeriod:create',
+  PayPeriodUpdate = 'PayPeriod:update',
+  PayPeriodDelete = 'PayPeriod:delete',
+
+  PositionRead = 'Position:read',
+  PositionCreate = 'Position:create',
+  PositionUpdate = 'Position:update',
+  PositionDelete = 'Position:delete',
+
+  RoleRead = 'Role:read',
+  RoleCreate = 'Role:create',
+  RoleUpdate = 'Role:update',
+  RoleDelete = 'Role:delete',
+
+  SalaryRead = 'Salary:read',
+  SalaryCreate = 'Salary:create',
+  SalaryUpdate = 'Salary:update',
+  SalaryDelete = 'Salary:delete',
+
+  ShiftRead = 'Shift:read',
+  ShiftCreate = 'Shift:create',
+  ShiftUpdate = 'Shift:update',
+  ShiftDelete = 'Shift:delete',
+
+  TimeLogRead = 'TimeLog:read',
+  TimeLogCreate = 'TimeLog:create',
+  TimeLogUpdate = 'TimeLog:update',
+  TimeLogDelete = 'TimeLog:delete',
+
+  TimeEntryRead = 'TimeEntry:read',
+  TimeEntryCreate = 'TimeEntry:create',
+  TimeEntryUpdate = 'TimeEntry:update',
+  TimeEntryDelete = 'TimeEntry:delete',
+
+  UserRead = 'User:read',
+  UserCreate = 'User:create',
+  UserUpdate = 'User:update',
+  UserDelete = 'User:delete',
+}
