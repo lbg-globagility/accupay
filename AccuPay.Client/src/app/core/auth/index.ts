4144d9e54b0c67d79818fc8f1d956d6dee5d99e4
aaron_delrosario <aaron.delrosario09@gmail.com>
2020-07-27 15:06:37 +0800

    feat(#111): add route guard for permissions

--- AccuPay.Client/src/app/core/auth/index.ts
+++ AccuPay.Client/src/app/core/auth/index.ts
@@ -2,3 +2,4 @@ export * from './auth.guard';
 export * from './auth.interceptor';
 export * from './auth.service';
 export * from './permission-types';
+export * from './permission.guard';
--- AccuPay.Client/src/app/main/main.component.ts
+++ AccuPay.Client/src/app/main/main.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from 'src/app/accounts/services/account.service';
-import { NgxPermissionsService } from 'ngx-permissions';
-import { Role } from 'src/app/roles/shared/role';
-import { LoadingState } from '../core/states/loading-state';
 
 @Component({
   selector: 'app-main',
@@ -10,53 +6,5 @@ import { LoadingState } from '../core/states/loading-state';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
-  state: LoadingState = new LoadingState();
-
-  constructor(
-    private accountService: AccountService,
-    private permissionsService: NgxPermissionsService
-  ) {}
-
-  ngOnInit(): void {
-    this.loadCurrentRole();
-  }
-
-  private loadCurrentRole(): void {
-    this.accountService.getCurrentRole().subscribe((role) => {
-      if (role == null) {
-        return;
-      }
-
-      this.addPermissions(role);
-      this.state.changeToSuccess();
-    });
-  }
-
-  private addPermissions(role: Role): void {
-    for (const rolePermission of role.rolePermissions) {
-      if (rolePermission.read) {
-        this.permissionsService.addPermission(
-          `${rolePermission.permissionName}:read`
-        );
-      }
-
-      if (rolePermission.create) {
-        this.permissionsService.addPermission(
-          `${rolePermission.permissionName}:create`
-        );
-      }
-
-      if (rolePermission.update) {
-        this.permissionsService.addPermission(
-          `${rolePermission.permissionName}:update`
-        );
-      }
-
-      if (rolePermission.delete) {
-        this.permissionsService.addPermission(
-          `${rolePermission.permissionName}:delete`
-        );
-      }
-    }
-  }
+  ngOnInit(): void {}
 }
