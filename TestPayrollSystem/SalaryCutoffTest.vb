d31146a42c8843bc9826fa9a5c7652fdec0ed6b2
aaron_delrosario <aaron.delrosario09@gmail.com>
2020-10-14 14:24:32 +0800

    test: test for salary cutoff

---
+++ TestPayrollSystem/SalaryCutoffTest.vb
@@ -0,0 +1,94 @@
+﻿Option Strict On
+
+Imports System.Collections.ObjectModel
+Imports AccuPay.Data.Entities
+Imports AccuPay.Data.ValueObjects
+
+<TestFixture>
+Public Class SalaryCutoffTest
+
+    <Test>
+    Public Sub ShouldSuccess_WhenSalariesIsWithinCutoff()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-01-01"), .BasicSalary = 100}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-01-01"), Date.Parse("2020-01-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result?.BasicSalary, [Is].EqualTo(100))
+    End Sub
+
+    <Test>
+    Public Sub ShouldReturnFirstSalary_WhenCutoffIsFirst()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-01-01"), .BasicSalary = 1000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-02-01"), .BasicSalary = 2000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-03-01"), .BasicSalary = 3000}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-01-01"), Date.Parse("2020-01-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result?.BasicSalary, [Is].EqualTo(1000))
+    End Sub
+
+    <Test>
+    Public Sub ShouldReturnMiddleSalary_WhenCutoffIsInMiddle()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-01-01"), .BasicSalary = 1000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-02-01"), .BasicSalary = 2000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-03-01"), .BasicSalary = 3000}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-02-01"), Date.Parse("2020-02-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result?.BasicSalary, [Is].EqualTo(2000))
+    End Sub
+
+    <Test>
+    Public Sub ShouldReturnLastSalary_WhenCutoffIsLast()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-01-01"), .BasicSalary = 1000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-02-01"), .BasicSalary = 2000},
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-03-01"), .BasicSalary = 3000}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-03-01"), Date.Parse("2020-03-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result?.BasicSalary, [Is].EqualTo(3000))
+    End Sub
+
+    <Test>
+    Public Sub ShouldReturnSalary_WhenSalaryIsBetweenCutoff()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-01-05"), .BasicSalary = 1000}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-01-01"), Date.Parse("2020-01-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result?.BasicSalary, [Is].EqualTo(1000))
+    End Sub
+
+    <Test>
+    Public Sub ShouldReturnNull_WhenSalaryIsAfterCutoff()
+        Dim salaries = New Collection(Of Salary) From {
+            New Salary() With {.EffectiveFrom = Date.Parse("2020-05-01"), .BasicSalary = 1000}
+        }
+        Dim cutoff = New TimePeriod(Date.Parse("2020-01-01"), Date.Parse("2020-01-15"))
+
+        Dim result = GetSalary(salaries, cutoff)
+
+        Assert.That(result, [Is].Null)
+    End Sub
+
+    Private Function GetSalary(salaries As ICollection(Of Salary), cutoff As TimePeriod) As Salary
+        Return salaries.Where(Function(s) s.EffectiveFrom <= cutoff.End).
+            OrderByDescending(Function(s) s.EffectiveFrom).
+            FirstOrDefault()
+    End Function
+
+End Class
