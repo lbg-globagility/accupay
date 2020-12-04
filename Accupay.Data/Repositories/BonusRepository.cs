f198d2373fb0608c0289501ee281d8a2db9e775f
Lambert Galit <lbg.globagility@gmail.com>
2020-12-04 09:37:49 +0800

    fix(#110): added function getting list of Bonus with Loan and LoaPaymentFromBonus.Items

--- Accupay.Data/Repositories/BonusRepository.cs
+++ Accupay.Data/Repositories/BonusRepository.cs
@@ -54,67 +54,6 @@ namespace AccuPay.Data.Repositories
                 .ToListAsync();
         }
 
-        public async Task<IEnumerable<Bonus>> GetBetweenDatesByEmployeeId(int employeeId, DateTime startDate)
-        {
-            var paystub = await _context.Paystubs
-                .Include(p => p.PayPeriod)
-                .Where(p => p.EmployeeID == employeeId)
-                .OrderByDescending(p => p.PayPeriod.Year)
-                .ThenByDescending(p => p.PayPeriod.OrdinalValue)
-                .FirstOrDefaultAsync();
-
-            var payperiod = paystub.PayPeriod;
-
-            var remainingPeriods = await _context.PayPeriods
-                .Where(pp => pp.OrganizationID == payperiod.OrganizationID)
-                .Where(pp => pp.Year == payperiod.Year)
-                .Where(pp => pp.PayFrequencyID == payperiod.PayFrequencyID)
-                .Where(pp => pp.OrdinalValue > payperiod.OrdinalValue)
-                .ToListAsync();
-
-            var minDate = remainingPeriods.Min(pp => pp.PayFromDate);
-            var maxDate = remainingPeriods.Max(pp => pp.PayToDate);
-
-            var bonuses = await _context.Bonuses
-                .Where(b => b.EmployeeID == employeeId)
-                .Where(b => b.EffectiveEndDate >= minDate)
-                .Where(b => b.EffectiveEndDate <= maxDate)
-                .ToListAsync();
-
-            return bonuses;
-        }
-
-        public async Task<IEnumerable<Bonus>> GetBetweenDatesNotFromIDs(int employeeId, DateTime startDate, int[] bonusIds)
-        {
-            var paystub = await _context.Paystubs
-                .Include(p => p.PayPeriod)
-                .Where(p => p.EmployeeID == employeeId)
-                .OrderByDescending(p => p.PayPeriod.Year)
-                .ThenByDescending(p => p.PayPeriod.OrdinalValue)
-                .FirstOrDefaultAsync();
-
-            var payperiod = paystub.PayPeriod;
-
-            var remainingPeriods = await _context.PayPeriods
-                .Where(pp => pp.OrganizationID == payperiod.OrganizationID)
-                .Where(pp => pp.Year == payperiod.Year)
-                .Where(pp => pp.PayFrequencyID == payperiod.PayFrequencyID)
-                .Where(pp => pp.OrdinalValue > payperiod.OrdinalValue)
-                .ToListAsync();
-
-            var minDate = remainingPeriods.Min(pp => pp.PayFromDate);
-            var maxDate = remainingPeriods.Max(pp => pp.PayToDate);
-
-            var bonuses = await _context.Bonuses
-                .Where(b => !bonusIds.Contains(b.RowID.Value))
-                .Where(b => b.EmployeeID == employeeId)
-                .Where(b => b.EffectiveEndDate >= minDate)
-                .Where(b => b.EffectiveEndDate <= maxDate)
-                .ToListAsync();
-
-            return bonuses;
-        }
-
         public async Task<ICollection<Bonus>> GetByPayPeriodAsync(int organizationId, TimePeriod timePeriod)
         {
             return await CreateBaseQueryByTimePeriod(organizationId, timePeriod).
@@ -126,6 +65,26 @@ namespace AccuPay.Data.Repositories
             return await CreateBaseQueryByTimePeriod(organizationId, timePeriod)
                 .Where(b => b.EmployeeID == employeeId)
                 .ToListAsync();
+        }
+
+        public async Task<ICollection<Bonus>> GetByEmployeeAndPayPeriodForLoanPaymentAsync(int organizationId, int employeeId, TimePeriod timePeriod)
+        {
+            return await CreateBaseQueryByTimePeriod(organizationId, timePeriod)
+                .Include(b => b.LoanPaymentFromBonuses)
+                    .ThenInclude(l => l.LoanSchedule)
+                .Include(b => b.LoanPaymentFromBonuses)
+                    .ThenInclude(l => l.Items)
+                .Where(b => b.EmployeeID == employeeId)
+                .Where(b => b.BonusAmount > 0)
+                .ToListAsync();
+        }
+
+        public async Task<Bonus> GetByIdAsync(int id)
+        {
+            return await _context.Bonuses
+                .Include(b => b.LoanPaymentFromBonuses)
+                .Where(b => b.RowID == id)
+                .FirstOrDefaultAsync();
         }
 
         private IQueryable<Bonus> CreateBaseQueryByTimePeriod(int organizationId, TimePeriod timePeriod)
