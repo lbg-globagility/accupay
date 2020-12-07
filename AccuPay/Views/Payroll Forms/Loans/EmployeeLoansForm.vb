2e371991548341bfd7f5e449640c06b97932ff0c
Lambert Galit <lbg.globagility@gmail.com>
2020-12-07 14:04:49 +0800

    fix(#110): checks LoanDeductFromBonus feature through PolicyHelper

--- AccuPay/Views/Payroll Forms/Loans/EmployeeLoansForm.vb
+++ AccuPay/Views/Payroll Forms/Loans/EmployeeLoansForm.vb
@@ -1,4 +1,4 @@
-﻿Option Strict On
+Option Strict On
 
 Imports System.Threading.Tasks
 Imports AccuPay.Data.Entities
@@ -49,6 +49,8 @@ Public Class EmployeeLoansForm
 
     Private _currentRolePermission As RolePermission
 
+    Private ReadOnly _policyHelper As PolicyHelper
+
     Sub New()
 
         InitializeComponent()
@@ -71,6 +73,8 @@ Public Class EmployeeLoansForm
 
         _userActivityRepository = MainServiceProvider.GetRequiredService(Of UserActivityRepository)
 
+        _policyHelper = MainServiceProvider.GetRequiredService(Of PolicyHelper)
+
         _textBoxDelayedAction = New DelayedAction(Of Boolean)
     End Sub
 
@@ -89,8 +93,7 @@ Public Class EmployeeLoansForm
 
         AddHandler SearchTextBox.TextChanged, AddressOf SearchTextBox_TextChanged
 
-        Dim checker = FeatureListChecker.Instance
-        lnkBonusPayment.Visible = checker.HasAccess(Feature.LoanDeductFromBonus)
+        lnkBonusPayment.Visible = _policyHelper.UseLoanDeductFromBonus
     End Sub
 
     Private Async Function CheckRolePermissions() As Task
