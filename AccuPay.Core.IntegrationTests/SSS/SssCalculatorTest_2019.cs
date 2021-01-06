413b17fd9993694ea566cff5c972348a30612e6d
Joshua Santos <jsantos.globagility@gmail.com>
2021-01-06 12:18:26 +0800

    feat: add tests for scenarios where the attendance cut off dates is not the same as the payroll cut off dates

--- AccuPay.Core.IntegrationTests/SSS/SssCalculatorTest_2019.cs
+++ AccuPay.Core.IntegrationTests/SSS/SssCalculatorTest_2019.cs
@@ -203,6 +203,203 @@ namespace AccuPay.Core.IntegrationTests.SSS
             //    Employee.EmployeeTypeDaily);
         }
 
+        [TestCaseSource(typeof(SSSTestSource_2019), "Brackets_SalaryBased")]
+        public void ShouldCalculate__WithIrregularPayPeriodAndBasicPayCalculationBasis(
+            decimal basicSalary,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: basicSalary,
+                SssCalculationBasis.BasicSalary,
+                previousTotalDaysPayWithOutOvertimeAndLeave: 0,
+                totalDaysPayWithOutOvertimeAndLeave: 0,
+                previousTotalEarnings: 0,
+                currentTotalEarnings: 0,
+                previousGrossPay: 0,
+                currentGrossPay: 0,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2019), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndBasicMinusDeductionsCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            var policyHelper = new Mock<IPolicyHelper>();
+            policyHelper.Setup(x => x.SssCalculationBasis).Returns(SssCalculationBasis.BasicMinusDeductions);
+
+            var paystubMock = new Mock<Paystub>();
+            paystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(totalDaysPayWithOutOvertimeAndLeave);
+            Paystub paystub = paystubMock.Object;
+            paystub.TotalEarnings = currentTotalEarnings;
+            paystub.GrossPay = currentGrossPay;
+
+            var previousPaystubMock = new Mock<Paystub>();
+            previousPaystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(previousTotalDaysPayWithOutOvertimeAndLeave);
+            Paystub previousPaystub = previousPaystubMock.Object;
+            previousPaystub.TotalEarnings = previousTotalEarnings;
+            previousPaystub.GrossPay = previousGrossPay;
+
+            var payPeriod = new PayPeriod()
+            {
+                PayFromDate = new DateTime(2019, 3, 16),
+                PayToDate = new DateTime(2019, 3, 31),
+                Half = PayPeriod.FirstHalfValue,
+                Year = 2019,
+                Month = 4
+            };
+
+            decimal basicSalary = 15_000m;
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeMonthly,
+                payPeriod);
+
+            decimal expectedSssEmployeeShareForFixed = 600m;
+            decimal expectedSssEmployerShareForFixed = 1_230m;
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShareForFixed,
+                expectedSssEmployerShare: expectedSssEmployerShareForFixed,
+                policyHelper.Object,
+                Employee.EmployeeTypeFixed,
+                payPeriod);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2019), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndGrossPayCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: 0,
+                SssCalculationBasis.Earnings,
+                previousTotalDaysPayWithOutOvertimeAndLeave: previousTotalDaysPayWithOutOvertimeAndLeave,
+                totalDaysPayWithOutOvertimeAndLeave: totalDaysPayWithOutOvertimeAndLeave,
+                previousTotalEarnings: previousTotalEarnings,
+                currentTotalEarnings: currentTotalEarnings,
+                previousGrossPay: previousGrossPay,
+                currentGrossPay: currentGrossPay,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2019), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndEarningsCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: 0,
+                SssCalculationBasis.GrossPay,
+                previousTotalDaysPayWithOutOvertimeAndLeave: previousTotalDaysPayWithOutOvertimeAndLeave,
+                totalDaysPayWithOutOvertimeAndLeave: totalDaysPayWithOutOvertimeAndLeave,
+                previousTotalEarnings: previousTotalEarnings,
+                currentTotalEarnings: currentTotalEarnings,
+                previousGrossPay: previousGrossPay,
+                currentGrossPay: currentGrossPay,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        /// <summary>
+        /// For scenarios like in Cinema 2000 and goldwings where the
+        /// payroll time period is different from attendance time period.
+        /// </summary>
+        private void BaseShouldCalculate_WithIrregularPayPeriod(
+            decimal basicSalary,
+            SssCalculationBasis sssCalculationBasis,
+            decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+            decimal totalDaysPayWithOutOvertimeAndLeave,
+            decimal previousTotalEarnings,
+            decimal currentTotalEarnings,
+            decimal previousGrossPay,
+            decimal currentGrossPay,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare)
+        {
+            var policyHelper = new Mock<IPolicyHelper>();
+            policyHelper.Setup(x => x.SssCalculationBasis).Returns(sssCalculationBasis);
+
+            var paystubMock = new Mock<Paystub>();
+            paystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(totalDaysPayWithOutOvertimeAndLeave);
+            Paystub paystub = paystubMock.Object;
+            paystub.TotalEarnings = currentTotalEarnings;
+            paystub.GrossPay = currentGrossPay;
+
+            var previousPaystubMock = new Mock<Paystub>();
+            previousPaystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(previousTotalDaysPayWithOutOvertimeAndLeave);
+            Paystub previousPaystub = previousPaystubMock.Object;
+            previousPaystub.TotalEarnings = previousTotalEarnings;
+            previousPaystub.GrossPay = previousGrossPay;
+
+            var payPeriod = new PayPeriod()
+            {
+                PayFromDate = new DateTime(2019, 3, 16),
+                PayToDate = new DateTime(2019, 3, 31),
+                Half = PayPeriod.FirstHalfValue,
+                Year = 2019,
+                Month = 4
+            };
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeFixed,
+                payPeriod);
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeMonthly,
+                payPeriod);
+        }
+
         private void BaseShouldCalculate(
             decimal basicSalary,
             Paystub paystub,
@@ -212,12 +409,6 @@ namespace AccuPay.Core.IntegrationTests.SSS
             IPolicyHelper policy,
             string employeeType)
         {
-            var sssRepository = MainServiceProvider.GetRequiredService<ISocialSecurityBracketRepository>();
-            IEnumerable<SocialSecurityBracket> socialSecurityBrackets = sssRepository.GetAll();
-
-            if (socialSecurityBrackets.Max(x => x.EffectiveDateFrom) < new DateTime(2020, 4, 1))
-                throw new Exception("SSS brackets for 2019-2020 are not created in the database yet");
-
             var payPeriod = new PayPeriod()
             {
                 PayFromDate = new DateTime(2019, 4, 1),
@@ -226,6 +417,33 @@ namespace AccuPay.Core.IntegrationTests.SSS
                 Year = 2019,
                 Month = 4
             };
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policy,
+                employeeType,
+                payPeriod);
+        }
+
+        private void BaseShouldCalculate(
+            decimal basicSalary,
+            Paystub paystub,
+            Paystub previousPaystub,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare,
+            IPolicyHelper policy,
+            string employeeType,
+            PayPeriod payPeriod)
+        {
+            var sssRepository = MainServiceProvider.GetRequiredService<ISocialSecurityBracketRepository>();
+            IEnumerable<SocialSecurityBracket> socialSecurityBrackets = sssRepository.GetAll();
+
+            if (socialSecurityBrackets.Max(x => x.EffectiveDateFrom) < new DateTime(2020, 4, 1))
+                throw new Exception("SSS brackets for 2019-2020 are not created in the database yet");
 
             var calculator = new SssCalculator(policy, socialSecurityBrackets.ToList(), payPeriod);
 
--- AccuPay.Core.UnitTests/SSS/SssCalculatorTest_2021.cs
+++ AccuPay.Core.UnitTests/SSS/SssCalculatorTest_2021.cs
@@ -201,7 +201,204 @@ namespace AccuPay.Core.UnitTests.SSS
             //    Employee.EmployeeTypeDaily);
         }
 
-        private static void BaseShouldCalculate(
+        [TestCaseSource(typeof(SSSTestSource_2021), "Brackets_SalaryBased")]
+        public void ShouldCalculate__WithIrregularPayPeriodAndBasicPayCalculationBasis(
+            decimal basicSalary,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: basicSalary,
+                SssCalculationBasis.BasicSalary,
+                previousTotalDaysPayWithOutOvertimeAndLeave: 0,
+                totalDaysPayWithOutOvertimeAndLeave: 0,
+                previousTotalEarnings: 0,
+                currentTotalEarnings: 0,
+                previousGrossPay: 0,
+                currentGrossPay: 0,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2021), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndBasicMinusDeductionsCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            var policyHelper = new Mock<IPolicyHelper>();
+            policyHelper.Setup(x => x.SssCalculationBasis).Returns(SssCalculationBasis.BasicMinusDeductions);
+
+            var paystubMock = new Mock<Paystub>();
+            paystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(totalDaysPayWithOutOvertimeAndLeave);
+            Paystub paystub = paystubMock.Object;
+            paystub.TotalEarnings = currentTotalEarnings;
+            paystub.GrossPay = currentGrossPay;
+
+            var previousPaystubMock = new Mock<Paystub>();
+            previousPaystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(previousTotalDaysPayWithOutOvertimeAndLeave);
+            Paystub previousPaystub = previousPaystubMock.Object;
+            previousPaystub.TotalEarnings = previousTotalEarnings;
+            previousPaystub.GrossPay = previousGrossPay;
+
+            var payPeriod = new PayPeriod()
+            {
+                PayFromDate = new DateTime(2020, 12, 16),
+                PayToDate = new DateTime(2020, 12, 31),
+                Half = PayPeriod.FirstHalfValue,
+                Year = 2021,
+                Month = 1
+            };
+
+            decimal basicSalary = 15_000m;
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeMonthly,
+                payPeriod);
+
+            decimal expectedSssEmployeeShareForFixed = 675m;
+            decimal expectedSssEmployerShareForFixed = 1_305m;
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShareForFixed,
+                expectedSssEmployerShare: expectedSssEmployerShareForFixed,
+                policyHelper.Object,
+                Employee.EmployeeTypeFixed,
+                payPeriod);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2021), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndGrossPayCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: 0,
+                SssCalculationBasis.Earnings,
+                previousTotalDaysPayWithOutOvertimeAndLeave: previousTotalDaysPayWithOutOvertimeAndLeave,
+                totalDaysPayWithOutOvertimeAndLeave: totalDaysPayWithOutOvertimeAndLeave,
+                previousTotalEarnings: previousTotalEarnings,
+                currentTotalEarnings: currentTotalEarnings,
+                previousGrossPay: previousGrossPay,
+                currentGrossPay: currentGrossPay,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        [TestCaseSource(typeof(SSSTestSource_2021), "Brackets_PaystubBased")]
+        public void ShouldCalculate_WithIrregularPayPeriodAndEarningsCalculationBasis(
+           decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+           decimal totalDaysPayWithOutOvertimeAndLeave,
+           decimal previousTotalEarnings,
+           decimal currentTotalEarnings,
+           decimal previousGrossPay,
+           decimal currentGrossPay,
+           decimal expectedSssEmployeeShare,
+           decimal expectedSssEmployerShare)
+        {
+            BaseShouldCalculate_WithIrregularPayPeriod(
+                basicSalary: 0,
+                SssCalculationBasis.GrossPay,
+                previousTotalDaysPayWithOutOvertimeAndLeave: previousTotalDaysPayWithOutOvertimeAndLeave,
+                totalDaysPayWithOutOvertimeAndLeave: totalDaysPayWithOutOvertimeAndLeave,
+                previousTotalEarnings: previousTotalEarnings,
+                currentTotalEarnings: currentTotalEarnings,
+                previousGrossPay: previousGrossPay,
+                currentGrossPay: currentGrossPay,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare);
+        }
+
+        /// <summary>
+        /// For scenarios like in Cinema 2000 and goldwings where the
+        /// payroll time period is different from attendance time period.
+        /// </summary>
+        private void BaseShouldCalculate_WithIrregularPayPeriod(
+            decimal basicSalary,
+            SssCalculationBasis sssCalculationBasis,
+            decimal previousTotalDaysPayWithOutOvertimeAndLeave,
+            decimal totalDaysPayWithOutOvertimeAndLeave,
+            decimal previousTotalEarnings,
+            decimal currentTotalEarnings,
+            decimal previousGrossPay,
+            decimal currentGrossPay,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare)
+        {
+            var policyHelper = new Mock<IPolicyHelper>();
+            policyHelper.Setup(x => x.SssCalculationBasis).Returns(sssCalculationBasis);
+
+            var paystubMock = new Mock<Paystub>();
+            paystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(totalDaysPayWithOutOvertimeAndLeave);
+            Paystub paystub = paystubMock.Object;
+            paystub.TotalEarnings = currentTotalEarnings;
+            paystub.GrossPay = currentGrossPay;
+
+            var previousPaystubMock = new Mock<Paystub>();
+            previousPaystubMock
+                .Setup(x => x.TotalDaysPayWithOutOvertimeAndLeave)
+                .Returns(previousTotalDaysPayWithOutOvertimeAndLeave);
+            Paystub previousPaystub = previousPaystubMock.Object;
+            previousPaystub.TotalEarnings = previousTotalEarnings;
+            previousPaystub.GrossPay = previousGrossPay;
+
+            var payPeriod = new PayPeriod()
+            {
+                PayFromDate = new DateTime(2020, 12, 16),
+                PayToDate = new DateTime(2020, 12, 31),
+                Half = PayPeriod.FirstHalfValue,
+                Year = 2021,
+                Month = 1
+            };
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeFixed,
+                payPeriod);
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policyHelper.Object,
+                Employee.EmployeeTypeMonthly,
+                payPeriod);
+        }
+
+        private void BaseShouldCalculate(
             decimal basicSalary,
             Paystub paystub,
             Paystub previousPaystub,
@@ -210,8 +407,6 @@ namespace AccuPay.Core.UnitTests.SSS
             IPolicyHelper policy,
             string employeeType)
         {
-            List<SocialSecurityBracket> socialSecurityBrackets = MockSocialSecurityBrackets.Get();
-
             var payPeriod = new PayPeriod()
             {
                 PayFromDate = new DateTime(2021, 1, 1),
@@ -220,6 +415,30 @@ namespace AccuPay.Core.UnitTests.SSS
                 Year = 2021,
                 Month = 1
             };
+
+            BaseShouldCalculate(
+                basicSalary: basicSalary,
+                paystub: paystub,
+                previousPaystub: previousPaystub,
+                expectedSssEmployeeShare: expectedSssEmployeeShare,
+                expectedSssEmployerShare: expectedSssEmployerShare,
+                policy,
+                employeeType,
+                payPeriod);
+        }
+
+        private void BaseShouldCalculate(
+            decimal basicSalary,
+            Paystub paystub,
+            Paystub previousPaystub,
+            decimal expectedSssEmployeeShare,
+            decimal expectedSssEmployerShare,
+            IPolicyHelper policy,
+            string employeeType,
+            PayPeriod payPeriod)
+        {
+            List<SocialSecurityBracket> socialSecurityBrackets = MockSocialSecurityBrackets.Get();
+
             var calculator = new SssCalculator(policy, socialSecurityBrackets, payPeriod);
 
             var salary = new Salary()
