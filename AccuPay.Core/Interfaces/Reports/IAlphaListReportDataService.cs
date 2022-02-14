88814b78a692848b9442fe5bcb1ab77f1efcfaf9
Lambert G <lbg.globagility@gmail.com>
2022-02-14 14:05:05 +0800

    feat(139): declare an interface for alphalist data service

---
+++ AccuPay.Core/Interfaces/Reports/IAlphaListReportDataService.cs
@@ -0,0 +1,16 @@
+using AccuPay.Core.Entities;
+using AccuPay.Core.ReportModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AccuPay.Core.Interfaces.Reports
+{
+    public interface IAlphaListReportDataService
+    {
+        Task<List<AlphalistModel>> GetData(
+            int organizationId,
+            bool actualSwitch,
+            PayPeriod startPeriod,
+            PayPeriod endPeriod);
+    }
+}
