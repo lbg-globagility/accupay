b54970b43839adca9fe56e95cf28273940d279f1
Lambert G <lbg.globagility@gmail.com>
2022-02-14 14:05:45 +0800

    feat(139): implement alphalist data service into class and supply functionality

---
+++ AccuPay.Infrastructure/Data/Reports/AlphaListReportDataService.cs
@@ -0,0 +1,64 @@
+using AccuPay.Core.Entities;
+using AccuPay.Core.Interfaces;
+using AccuPay.Core.Interfaces.Reports;
+using AccuPay.Core.ReportModels;
+using AccuPay.Core.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccuPay.Infrastructure.Data.Reports
+{
+    public class AlphaListReportDataService : StoredProcedureDataService, IAlphaListReportDataService
+    {
+        private readonly IPaystubRepository _paystubRepository;
+
+        public AlphaListReportDataService(PayrollContext context,
+            IPaystubRepository paystubRepository) : base(context)
+        {
+            _paystubRepository = paystubRepository;
+        }
+
+        public async Task<List<AlphalistModel>> GetData(int organizationId,
+            bool actualSwitch,
+            PayPeriod startPeriod,
+            PayPeriod endPeriod)
+        {
+            var periodRange = new TimePeriod(startPeriod.PayFromDate,
+                endPeriod.PayToDate);
+
+            var paystubs = await _context.Paystubs
+                .Include(x => x.Actual)
+                .Include(x => x.PayPeriod)
+                .Include(x => x.ThirteenthMonthPay)
+                .Include(x => x.Employee)
+                .Where(x => x.OrganizationID == organizationId)
+                .Where(x => x.PayPeriod.PayFromDate >= periodRange.Start)
+                .Where(x => x.PayPeriod.PayToDate <= periodRange.End)
+                .ToListAsync();
+
+            var latestSalaries = GetLatestSalaries(organizationId);
+
+            return paystubs.GroupBy(p => p.EmployeeID).
+                OrderBy(p => p.FirstOrDefault().Employee.FullNameLastNameFirst).
+                Select(p => new AlphalistModel(paystubs: p,
+                    startingDate: periodRange.Start,
+                    latestSalaries: latestSalaries,
+                    actualSwitch: actualSwitch)).
+                    ToList();
+        }
+
+        private List<SalaryModel> GetLatestSalaries(int organizationId)
+        {
+            string procedureSql = $"CALL `GetLatestSalaries`({organizationId});";
+            var result = CallRawSql(procedureSql);
+
+            return result.Rows.
+                OfType<DataRow>().
+                Select(r => new SalaryModel(dataRow: r)).
+                ToList();
+        }
+    }
+}
