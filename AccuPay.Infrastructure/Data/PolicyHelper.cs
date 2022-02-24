c7cbd7eae20340b79779d5c9f321c2a659dcf315
Lambert G <lbg.globagility@gmail.com>
2022-02-24 16:50:48 +0800

    feat(140a): access Leave Policy and Leave Reset Policy in Policy helper

--- AccuPay.Infrastructure/Data/PolicyHelper.cs
+++ AccuPay.Infrastructure/Data/PolicyHelper.cs
@@ -214,5 +214,11 @@ namespace AccuPay.Infrastructure.Data
         }
 
         #endregion Pay Period Default Dates Policy ("16,31,false,true,false,false" means cutoff start day is "16", cutoff end day is "31", first day "is NOT last day of the month", second day "is last day of the month", first day "is not previous month", second day "is not previous month"
+
+        public ILeavePolicy GetLeavePolicy => new LeavePolicy(settings: _settings);
+
+        public ILeaveResetPolicy GetLeaveResetPolicy => new LeaveResetPolicy(settings: _settings);
+
+        public bool IsEnableCashoutUnusedLeaves => _settings.GetBoolean("LeaveConvertiblePolicy.Enable");
     }
 }
